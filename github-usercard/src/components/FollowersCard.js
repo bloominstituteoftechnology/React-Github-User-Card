2400835527763fe9fe441727bea213eeb0a73a10
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-08-06 19:21:50 -0600

    Added axios call to FollowersCard.js

--- github-usercard/src/components/FollowersCard.js
+++ github-usercard/src/components/FollowersCard.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
 class FollowersCard extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
             followers: []
         }
     }
 
     //https://api.github.com/users/jadeli1720/followers
+    fetchFollowers = () => {
+        axios
+        .get(``)
+        .then(res => {
+            this.setState({users: res.data})
+        })
+        .catch(err => console.log('Sorry, the API is currently down', err))
+    }
 
     render() {
         return(
