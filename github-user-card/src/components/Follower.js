de23f719ed451d3882bf0edde36ca0d3a1657794
Kristin Barr <kristinbarr@gmail.com>
2019-08-07 12:29:14 -0400

    rainbow styling updates

--- github-user-card/src/components/Follower.js
+++ github-user-card/src/components/Follower.js
@@ -4,10 +4,12 @@ class Follower extends Component {
   render() {
     const { login, avatar_url, html_url } = this.props.follower
     return (
-      <div className='follower'>
-        <img src={avatar_url} alt='avatar' />
-        <h3>{login}</h3>
-        <a href={html_url}>Github</a>
+      <div className='rainbow-wrapper'>
+        <div className='follower'>
+          <img src={avatar_url} alt='avatar' />
+          <h3>{login}</h3>
+          <a href={html_url}>Github</a>
+        </div>
       </div>
     )
   }
