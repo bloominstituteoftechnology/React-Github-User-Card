2adfe6c4575df741d728e3dfbe75d41539486f88
adriancobon <adriancobon@outlook.com>
2019-09-02 16:10:42 -0400

    Finalized project

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
- import Header from './components/Header';
+ import Header from './components/Header.js';
 import Card from './components/Card.js';
 import { Container } from 'shards-react';
 
