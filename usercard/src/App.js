7d268045358da4ea5f91354323915bde50490738
adriancobon <adriancobon@outlook.com>
2019-09-02 15:27:35 -0400

    Typed app.js and manipulated app.css

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
+
+ import Header from './components/Header';
+import Card from './components/Card';
+import { Container } from 'shards-react';
+
+ import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      ghMyData: null,
+      ghData: null,
+    }
+  }
+
+   componentDidMount() {
+    this.fetchData();
+    this.fetchMyData();
+  }
+
+   fetchMyData = () => {
+    fetch('https://api.github.com/users/danbergelt')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        return this.setState({ ghMyData: data})
+      })
+      .catch(err => console.log(err))
+  }
+
+
+   fetchData = () => {
+    fetch('https://api.github.com/users/danbergelt/followers')
+      .then(res => {
+        return res.json()
+      })
+      .then(data => {
+        return this.setState({ ghData: data })
+      })
+      .catch(err => console.log(err))
+  }
+
+
+   render() {
+    return (
+      <div className="App">
+        <Header />
+        {!this.state.ghData && <h1 className="loader">Loading users...</h1>}
+        <Container className="card-container">
+        {console.log(this.state.ghMyData)}
+        {console.log(this.state.ghData)}
+        {this.state.ghMyData
+        &&
+          <Card
+            key={this.state.ghMyData.id}
+            username={this.state.ghMyData.login}
+            img={this.state.ghMyData.avatar_url}
+            repos={this.state.ghMyData.html_url}
+          />
+        }
+        {this.state.ghData     
+        &&
+        this.state.ghData.map(userData => (
+          <Card
+            key={userData.id}
+            username={userData.login}
+            img={userData.avatar_url}
+            repos={userData.html_url}
+          />
+        ))
+        }
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default App;
+ export default App;
