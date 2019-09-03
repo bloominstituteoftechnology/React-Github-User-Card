4af00c7bb3fa4e5ba7031f16de1f4c6ef78cf666
Christopher Hernandez <cerberean661@gmail.com>
2019-09-02 23:30:30 -0700

    first commit after setup

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super();
-    this.state - {
+    this.state = {
       user: {},
     };
   }
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/ChristopherHernandezW22')
+    .then(res => res.json())
+    .then(data => console.log(data));
+  }
+
   render() {
+
     return (
       <div className="App">
     </div>
