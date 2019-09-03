2a7c2afcd78c176d2d9369317d2a56dd897fba5c
Christopher Hernandez <cerberean661@gmail.com>
2019-09-02 23:53:51 -0700

    stuck on listing followers. error on line 42 in App.js

--- src/App.js
+++ src/App.js
@@ -6,22 +6,46 @@ class App extends React.Component {
     super();
     this.state = {
       user: {},
+      follower: []
     };
   }
 
   componentDidMount() {
+    console.log("First Render (mounting)");
     fetch('https://api.github.com/users/ChristopherHernandezW22')
-    .then(res => res.json())
-    .then(data => console.log(data));
+      .then(res => res.json())
+      .then(data => this.setState({ user:data }));
+    fetch('https://api.github.com/users/followers')
+      .then(res => res.json())
+      .then(data => this.setState({ follower:data }));
+  }
+
+  componentDidUpdate () {
+    console.log(this.state);
   }
 
   render() {
-
     return (
       <div className="App">
+        <UserCard user={this.state.user} followers={this.state.followers} />
     </div>
     );
   }
+}
+
+function UserCard(props) {
+  return (
+    <div>
+      <h2>{props.user.login}</h2>
+      <p>{props.user.location}</p>
+      <p>{props.user.url}</p>
+      <div>
+          {props.followers.map(follower => ( 
+            <div key={follower.id}>{follower.login}</div>
+          ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
