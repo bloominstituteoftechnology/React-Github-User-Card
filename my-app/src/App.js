7bdfab1b325f479b82cc1507ba006faebadf983b
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-09-03 17:13:53 -0400

    got user coming through kind of and data in having little trouble with avater pulling in

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,50 @@
-import React from 'react';
-import Github from "./Github.js";
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import User from "../src/User.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-<Github>
-</Github>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [],
+      userFollowers: []
+    };
+  }
+
+  componentDidMount() {
+    this.getUsers();
+    this.getFollowers();
+  }
+
+  getUsers = () => {
+    axios
+      .get("https://api.github.com/user/strimnal32246")
+      .then(res => this.setState({ userData: res.data }))
+      .catch(err => {
+        console.log("error", err);
+      });
+  };
+
+  getFollowers = () => {
+    axios
+      .get("https://api.github.com/users/strimnal32246/followers")
+      .then(res => this.setState({ userFollowers: res.data }))
+      .catch(err => {
+        console.log("error", err);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {console.log(this.state.userFollowers)}
+        </header>
+        <User
+          userData={this.state.userData}
+          userFollowers={this.state.userFollowers}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
