f58dc787fe3d2b1a64491a605369666cf9311df7
Christopher Hernandez <cerberean661@gmail.com>
2019-09-02 21:30:45 -0700

    deleting files and setting up App.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state - {
+      user: {},
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+    </div>
+    );
+  }
+}
+
+export default App;
