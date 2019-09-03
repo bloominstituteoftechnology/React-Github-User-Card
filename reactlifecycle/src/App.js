66543ef94a31f1f948a1027c058f1ba5ae49c968
Connor Holly <connorholly11@gmail.com>
2019-09-03 15:57:55 -0400

    fetched data and setState

--- reactlifecycle/src/App.js
+++ reactlifecycle/src/App.js
@@ -2,15 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    console.log('constructor invoked!')
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount(){
+    console.log('CDM invoked!')
+    fetch('https://api.github.com/users/connorholly11')
+    .then(response => response.json())
+    .then((response) => console.log(response))
+    .then(card => this.setState({users: card}) )
+    
+  }
+
+
+  render() {
+  console.log('render invoked!')
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World </h1>
+        <h1>Github Card</h1>
       </header>
     </div>
   );
+}
 }
 
 export default App;
