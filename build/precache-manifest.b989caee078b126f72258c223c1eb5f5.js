c66d40dd8a9c233c74ee690a41a39883a75b7e7d
Benjamin <benjamindoyle97@gmail.com>
2019-09-05 22:17:00 -0700

    added build

---
+++ build/precache-manifest.b989caee078b126f72258c223c1eb5f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c74c8e0f63d7467e404c89e6a2abaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8c13083ef0bb16746c1",
+    "url": "/static/css/main.f729c01e.chunk.css"
+  },
+  {
+    "revision": "ea381fc7f35076972ee8",
+    "url": "/static/js/2.22466e86.chunk.js"
+  },
+  {
+    "revision": "f8c13083ef0bb16746c1",
+    "url": "/static/js/main.3328a1c6.chunk.js"
+  },
+  {
+    "revision": "5b507a94c11931b13aae",
+    "url": "/static/js/runtime~main.90f363c0.js"
+  }
+]);
