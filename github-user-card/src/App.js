826d3fdddaec96a23f807a7187a6129bfcca12e1
Jashele T <jashele@Jasheles-Air.domain>
2019-09-06 21:56:20 -0700

    styled

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -17,13 +17,12 @@ class App extends React.Component {
         console.log("hello???")
         fetch('https://api.github.com/users/jasheloper')
         .then(res => res.json())
-        .then(res => this.setState({ people: res }))
+        .then(res => this.setState({people: res}))
         .catch(err => console.log(err));
 
         fetch('https://api.github.com/users/jasheloper/followers')
- 
         .then(res => res.json())
-        .then(res => this.setState({ followers: res }))
+        .then(res => this.setState({followers: res}))
         .catch(err => console.log(err));
     }
 
@@ -35,50 +34,78 @@ class App extends React.Component {
             <div class="container">
 
                 <div class="name">
-                    {this.state.people.name}
+                    {
+                    this.state.people.name
+                }
+
+                    <h1>GitHub User Card</h1>
                 </div>
 
 
-                <h3 class="bio">
-
-                {this.state.people.bio}
-                </h3>
-
-
-                <img class="jashele" src={
-                    this.state.people.avatar_url
-                } alt="Jashele Tillman - jasheloper on Github" />
-
-<p>Followers:  {this.state.people.followers}</p>
-
-<p>Following  {this.state.people.following}</p>
+                <div class="intro">
+                    <div class="intro-box">
+                        {
+                        this.state.people.bio
+                    } </div>
 
 
+                    <div class="intro-box">
 
-{this.state.followers.map(x => 
-  <img src={x.avatar_url} /> )}
+                        <img src={
+                                this.state.people.avatar_url
+                            }
+                            alt="Jashele Tillman - jasheloper on Github"/>
+
+                    </div>
+                </div>
 
 
+                <div class="followbox">
 
-</div>
+                    <div class="followboxee">
+                        <p>Followers: {
+                            this.state.people.followers
+                        }</p>
+
+                    </div>
+
+                    <div class="followboxee">
+                        <p>Following {
+                            this.state.people.following
+                        }</p>
+
+                    </div>
 
 
+                </div>
+
+                <div class="people">
+
+                    {
+                    this.state.followers.map(x => {
+                        return (
 
 
+                            <div class="person">
+                                <img src={
+                                    x.avatar_url
+                                }/>
+                                <p>{
+                                    x.login
+                                }</p>
+                            </div>
 
-           
+
+                        )
+                    })
+                } </div>
+
+            </div>
+
+
         );
     }
 }
-
-
-
-
-
-
-
-
-
 
 
 export default App;
