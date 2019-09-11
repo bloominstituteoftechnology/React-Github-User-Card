db40484bd94b1205e3aa0441416efa68246a315a
Eric <eric@Erics-MacBook-Pro.local>
2019-09-10 19:50:40 -0500

    MVP

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import axios from 'axios';
-// import UserCard from './components/UserCard';
+import 'semantic-ui-css/semantic.min.css';
 
 import './App.css';
-
+import UserCard from './components/UserCard';
+import CardList from './components/CardList';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state ={
-      MyData: null,
-      MyFollowers: null
+      MyData: [],
+      MyFollowers: []
     }
   }
 
@@ -18,7 +18,6 @@ componentDidMount() {
   this.FetchMyData();
   this.FetchMyFollowers();
 }
-
 
 FetchMyData = () => {
   fetch('https://api.github.com/users/eric-wise')
@@ -42,21 +41,17 @@ FetchMyFollowers = () => {
   .catch(err => console.log(err))
 }
 
-
-
-
-
-
 render() {
+  // console.log(this.state.MyData)
   return (
     <div className='App'>
-      {console.log(this.state.MyData)}
-      {console.log(this.state.MyFollowers)}
+      <h1>GitHub User Card</h1>
+      <UserCard MyData = {this.state.MyData} />
+      <CardList MyFollowers = {this.state.MyFollowers} />
+      {/* {console.log(this.state.MyData)} */}
 
-
-
-
-
+      {/* {console.log(this.state.MyFollowers)} */}
+      {/* <UserCard MyData={this.state.MyData} /> */}
     </div>
   )
 }
--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Card, Container, Image, Icon } from 'semantic-ui-react';
 
 
 
-export default function UserCard()
+    
+const UserCard = (props) => {
+  // console.log(props.MyData.name)
+        return (
+          <div>
+            <Container>              
+              <Card centered>
+                <Card.Content>
+                  <Card.Header>{props.MyData.name}</Card.Header>
+                </Card.Content>
+                <Card.Content>
+                  <Image size="small" className="MyPic" src={props.MyData.avatar_url} wrapped ui={false} />
+                  <Card.Header className="header"><Icon name="github" /> {props.MyData.login}</Card.Header>
+                  <Card.Meta><Icon name="home" /> {props.MyData.location}</Card.Meta>
+                  <Card.Description>{props.MyData.bio}</Card.Description>
+                  <Icon name='group' />{props.MyData.followers} Followers
+                </Card.Content>                           
+              </Card>
+            </Container>
+          </div>
+        )
+      }
+      
+    
 
-
-
-return (
-    <Card>
-
-    </Card>
-)
+export default UserCard;
