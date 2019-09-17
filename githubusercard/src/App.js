69a069f9817103cecc6f724a962071976af824b0
Shantay Grady <shantaygrady@gmail.com>
2019-09-16 21:05:55 -0400

    sets state

--- githubusercard/src/App.js
+++ githubusercard/src/App.js
@@ -4,16 +4,28 @@ import userCard from './components/usercard';
 import './App.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    console.log("Constructor Invoked");
+    super();
+    this.state = {
+      users: []
+    };
+  }
+  componentDidMount() {
+    console.log("CDM Invoked!");
+    this.setState({ users: users })
   }
 
   render() {
     return (
       <div className="App">
         <h1>Welcome to Github User Card!</h1>
+        </div>{this.state.users.map(user => (
+          <div key={user.id}>
+            {user.first_name + user.email}
+        ))}
       </div>
-    );
+    ); 
   }
 }
 
