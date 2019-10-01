617b243a49f1b359be1537326abe4ecf44924b06
Kevin Allen <kevin.allen91@gmail.com>
2019-10-01 16:12:29 -0700

    mvp

--- git-usercard/src/App.js
+++ git-usercard/src/App.js
@@ -5,32 +5,65 @@ import UserCard from "./components/user-card.js"
 
 
 class App extends React.Component {
-  state = {
-    users:[]
+  constructor(){
+    super();
+    this.state = {
+      users:[],
+      followers:[],
+      followerData:[]
   }
+  }
+  
+  
 
   componentDidMount() {
     axios.get(`https://api.github.com/users/kmallen91`)
     .then (res => {
-      console.log(res)
+      console.log(res.data.followers_url)
       this.setState ({
         users:res.data
       })
     })
-    .catch(err => console.log('ERROR'))
+    .catch(err => console.log('ERROR'));
+    axios.get(`https://api.github.com/users/kmallen91/followers`)
+    .then (res => {
+      console.log(res.data)
+      this.setState({
+        followers: res.data
+      })
+      })
+    .catch(err => console.log('ERROR'));
+    this.state.followers.forEach(e => {
+    axios.get(`https://api.github.com/users/${e.login}`)
+    .then (res => {
+      console.log(res.data)
+        this.setState({
+          followerData:res.data
+        
+        })
+        
+      })            
+    .catch(err => console.log('ERROR'));
+    })
   };
+
+
 
 render() {
 return (
+
     <div className="App">
       <h2> Placeholder </h2>
       <UserCard 
       data={this.state.users}
       />
-
+      <UserCard 
+      data={this.state.followerData}
+      />
     </div>
   );
 }
+
 }
 
 export default App;
--- git-usercard/src/components/user-card.js
+++ git-usercard/src/components/user-card.js
@@ -1,17 +1,31 @@
 import React from "react"
 
 const UserCard = (props) => {
-console.log(props)
+console.log(props.followers)
+// followers = props.followers
     return (
-        
-            <div key={props.data.id} className="user">
-            <img src ={props.data.avatar_url} alt="profile picture"/>
+        <div>
+        <div key={props.data.id} className="user">
+            <img src ={props.data.avatar_url} alt="profile"/>
             <h2> {props.data.name} </h2>
             <h3> Location: {props.data.location} </h3>
             <p> Followers: {props.data.followers} </p>
             <p> Following: {props.data.following} </p>
 
-            </div>
+        </div>
+    {/* <div className="followers-container" >
+    
+      
+        <div className="followers" key={followers.id}>
+          <img width="200" src={followers.avatar_url}  alt={followers.name} />
+          <h2> {followers.name} </h2>
+            <h3> Location: {followers.location} </h3>
+            <p> Followers: {followers.followers} </p>
+            <p> Following: {followers.following} </p>
+        </div>
+        
+    </div> */}
+    </div>
         
     )
 }
