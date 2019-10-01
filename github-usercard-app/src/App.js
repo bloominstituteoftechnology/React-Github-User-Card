3c86def192f02e77193b943c48964b07c6d1eb28
Jennie Van <missjenvan@gmail.com>
2019-10-01 13:41:49 -0700

    fixed compiler error

---
+++ github-usercard-app/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+
+class App extends React.Component {
+  state = {
+    user: "YenniLee",
+    userData: {},
+    followers: []
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        
+        
+      </div>
+    );
+  }
+  
+}
+
+export default App;
