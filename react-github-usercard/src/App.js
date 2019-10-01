3694d675ccca21f86801f39c50754001602591a9
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-01 15:34:47 +0300

    created userlist component

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UserList from './component/UserList';
 
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+
+    }
   }
   render() {
     return(
       <div className="App">
+      Hello from App!
+      <UserList/>
       
       </div>
     )
---
+++ react-github-usercard/src/component/UserList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+ class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div>Hello from Userlist!</div>;
+  }
+}
+
+export default UserList;
