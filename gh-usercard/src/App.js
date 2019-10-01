2f660595929515a3782f18407e8ebd8e81aa9b9f
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-10-01 14:01:23 -0600

    added axios call

--- gh-usercard/src/App.js
+++ gh-usercard/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+class App extends React.Component{
+  state={
+    follower:[],
+
+  }
+  componentDidMount(){
+    Axios
+    .get('https://api.github.com/users/a-soren/followers')
+    .then(res=>{
+      console.log('axios call response', res);
+    })
+    .catch(err=>{
+      console.log('Error: ', err);
+    })
+  }
 }
 
 export default App;
