b75263e1b1f80a81d7b81a1a80a470af04ebe44c
Mildred Awuor <otieno.awuor.m@gmail.com>
2019-10-02 08:28:35 +0300

    imported material UI

--- react-github-usercard/src/component/Followers.js
+++ react-github-usercard/src/component/Followers.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Grid from "@material-ui/core/Grid";
 
 class Followers extends React.Component {
   constructor(props) {
@@ -6,21 +7,21 @@ class Followers extends React.Component {
   }
   render() {
     return (
-      <div className="followers">
-        {this.props.followers.map(followers => {
-          console.log("followers", followers);
-          return (
-            <div className="following" key={followers.id}>
-              <img className="image" src={followers.avatar_url}></img>
-              <div>
-                <p>Github url: {followers.url}</p>
-                <p>Username: {followers.login}</p>
-                <p>repos: {followers.repos_url}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      <Grid className="followers" container direction="column" spacing={2} justify="space-between" alignItems="center">
+          {this.props.followers.map(followers => {
+            console.log("followers", followers);
+            return (
+              <Grid className="following" container direction="row" justify="center" alignItems="center"   key={followers.id}>
+                <img className="image" src={followers.avatar_url}></img>
+                <div>
+                  <p>Github url: {followers.url}</p>
+                  <p>Username: {followers.login}</p>
+                  <p>repos: {followers.repos_url}</p>
+                </div>
+              </Grid>
+            );
+          })}
+      </Grid>
     );
   }
 }
