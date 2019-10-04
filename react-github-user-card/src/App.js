09560e33b1afeb4da16f2dd91bca71ab7c11560d
Amber-Pittman <amber@amberpittman.com>
2019-10-04 19:55:43 -0400

    Added my App code back on

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            //set initial default state values
+            name: "Amber Pittman",
+            username: "Amber-Pittman",
+            location: " || 'Not Available' ",
+        }
+
+    }
+
+    
+    render() {
+        return (
+            <div class="card">
+                <img src={image url of user} data-pin-nopin="true" />
+                <div class="card-info">
+                    <h3 class="name">{users name}</h3>
+                    <p class="username">{users user name}</p>
+                    <p>Location: {users location}</p>
+                    <p>Profile:  
+                    <a href={address to users github page}>{address to users github page}</a>
+                    </p>
+                    <p>Followers: {users followers count}</p>
+                    <p>Following: {users following count}</p>
+                    <p>Bio: {users bio}</p>
+                </div>
+            </div>
+        )
+    }
 }
+
 
 export default App;
