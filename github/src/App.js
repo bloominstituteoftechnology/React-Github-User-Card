8e589794629488aef20312442518f6b84e61e340
Veto Ramirez <vetorobert3@gmail.com>
2019-11-02 10:09:06 -0700

    added card.js file

---
+++ github/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return(
+      <h1>GitHub Users</h1>
+    )
+  }
+  
+}
+
+export default App;
