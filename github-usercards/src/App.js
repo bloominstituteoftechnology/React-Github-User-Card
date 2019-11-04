eab2ce65718f66fa61ff0233a2292e0b9999107c
adkhiker <poirier.greg@gmail.com>
2019-11-03 23:55:40 -0500

    Fetch data

--- github-usercards/src/App.js
+++ github-usercards/src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import GitHubUserCard from './components/GitHubUserCard';
+import PageHeader from './components/PageHeader';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      user: {},
+      followersLinks: [],
+      followers: [],
+    };
+  }
+
+  componentDidMount = () => {
+    fetch("https://api.github.com/users/adkhiker")
+      .then(response => response.json())
+      .then(data => this.setState({ ...this.state, user: data }));
+    fetch("https://api.github.com/users/adkhiker/followers")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ ...this.state, followersLinks: [...data] });
+        this.state.followersLinks.forEach(follower => {
+          fetch(`${follower.url}`)
+            .then(response => response.json())
+            .then(data =>
+              this.setState({
+                ...this.state,
+                followers: [...this.state.followers, data]
+              })
+            );
+        });
+      });
+  }
+  
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PageHeader />
+      <GitHubUserCard followers={this.state.followers} user={this.state.user} />
+      <div class="graph">
+        <p>Contributions for Adkhiker:</p>
+        <img
+          src="http://ghchart.rshah.org/adkhiker"
+          alt="Github Chart for Adkhiker"
+          title="Github Chart for Adkhiker"
+        />
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
