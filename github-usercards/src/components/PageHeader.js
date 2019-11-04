d29d30d8c446e21f124bc2b65c90d354e967112f
adkhiker <poirier.greg@gmail.com>
2019-11-03 23:58:35 -0500

    WIP: Display user

---
+++ github-usercards/src/components/PageHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import githublogo from '../assets/githublogo.png';
+import lambdalogo from '../assets/lambdalogo.png';
+import { Header, Image } from 'semantic-ui-react'
+const PageHeader = () => {
+    return <Header as='h1' style={{display: 'flex', justifyContent: 'space-around', width: '50%', margin: 'auto'}}>
+        <Image src={lambdalogo} />
+        ❤️'s
+       <Image src={githublogo} />
+    </Header>
+}
+
+export default PageHeader;
