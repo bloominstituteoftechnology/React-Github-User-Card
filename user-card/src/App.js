be772fb038e2d4ba16c71c95f3ce6725961b8a18
squashgray <squashgray@gmail.com>
2019-11-27 14:53:49 -0800

    initial commit

---
+++ user-card/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>HI IM APP</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
