a5c79e85ef945c9e34d9bcb3fd95ee1b9635e721
wais <waisalmakaleh@gmail.com>
2019-12-03 18:05:23 -0500

    git it to work

--- githhub-cards/src/App.js
+++ githhub-cards/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+      followers: []
+    }
+  }
+  componentDidMount(){
+    axios.get('https://api.github.com/users/Wais-A/followers')
+    .then(res => {
+      console.log(res.data)
+      this.setState({followers: res.data})
+    })
+  }
+
+  render(){
+    return(
+      <div className='app'>
+        {this.state.followers.map((item, key)=>{
+          return(
+            <div className='followerInfo' key={key} alt={item.login}>
+              <img src={item.avatar_url}/>
+              <h1>{item.login}</h1>
+            </div>)
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
