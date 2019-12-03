abcd49c6680a26b87ef29622f1649fc6dab5f6fe
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-03 17:33:36 -0500

    rendering profile info and list of followers with usernames

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,52 +1,60 @@
-import React from 'react';
-import axios from 'axios'
-import GitCard from './components/GitCard'
-import './App.css';
+import React from "react";
+import axios from "axios";
+import GitCard from "./components/GitCard";
+import "./App.css";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       user: {},
       followers: []
-    }
+    };
   }
 
-    componentDidMount() {
-      this.fetchApiData()
-    
+  componentDidMount() {
+    this.fetchApiData();
   }
 
-  fetchApiData = userName => {
+  fetchApiData = username => {
     axios
       .get(`https://api.github.com/users/Lfritze`)
       .then(res => {
-        this.setState({ user: res.data});
+        this.setState({ user: res.data });
 
-        axios.get(res.data.followers.followers_url)
-        .then(followerInfo => {
-          this.setState({ followers: followerInfo.data });
-        })
-        .catch(followError => {
-          console.log('error in follower fetch in App.js', followError);
-        });
+        axios
+          .get(res.data.followers_url)
+          .then(followerInfo => {
+            this.setState({ followers: followerInfo.data });
+          })
+          .catch(followError => {
+            console.log("error in follower fetch in App.js", followError);
+          });
       })
       .catch(err => {
-        console.log('error in request in App.js', err)
-      })
-  }
+        console.log("error in request in App.js", err);
+      });
+  };
 
   render() {
-  return (
-    <div className="App">
-      <GitCard 
-         avatarUrl={this.state.user.avatar_url}
-         profileUrl={this.state.user.html_url}
-      />
-      
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <GitCard
+          avatarUrl={this.state.user.avatar_url}
+          name={this.state.user.name}
+          username={this.state.user.login}
+          profileUrl={this.state.user.html_url}
+          location={this.state.user.location}
+          profileUrl={this.state.user.html_url}
+          repos={this.state.user.public_repos}
+          followers={this.state.user.followers}
+          following={this.state.user.following}
+          bio={this.state.user.bio}
+          followerList={this.state.followers}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- github-cards/src/components/GitCard.js
+++ github-cards/src/components/GitCard.js
@@ -1,26 +1,58 @@
-import React from 'react'
-import GithubCalendar from 'github-calendar'
+import React from "react";
+import GithubCalendar from "github-calendar";
 
 class GitCard extends React.Component {
-
-    componentDidUpdate(prevProps) {
-        if (prevProps.username !== this.props.username) {
-            GithubCalendar('.calendar', this.props.username, { responsive: true})
-            //previousProps go with componentDidUpdate
-            // UserName is a parameter in FetchData in App.js
-        }
+  componentDidUpdate(prevProps) {
+    if (prevProps.username !== this.props.username) {
+      GithubCalendar(".calendar", this.props.username, { responsive: true });
+      //previousProps go with componentDidUpdate
+      // Username is a parameter in axios get in App.js
     }
-    render() {
-        return (
-            <div className='git-card-container'>
-                <div className='user'>
-                    <a  href={this.props.profileUrl} target='_blank' rel='noopener noreferrer'> 
-                        <img src={this.props.avatarUrl} alt='avatar name' />
-                    </a>
+  }
+  render() {
+    return (
+      <div className="git-card-container">
+        <div className="user">
+          <a
+            href={this.props.profileUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={this.props.avatarUrl} alt="avatar name" />
+          </a>
+          <div className="user-info">
+            <h1>{this.props.name}</h1>
+            <p>{this.props.username}</p>
+            <p>{this.props.location}</p>
+            <p>Repositories: {this.props.repos}</p>
+            <p>Followers: {this.props.followers}</p>
+            <p>Following: {this.props.following}</p>
+            <p>Bio: {this.props.bio}</p>
+          </div>
+          <div className="contributin-chart">
+            <div className="followers">
+              <h2>Followers</h2>
+              {this.props.followerList.map((follower, idx) => (
+                <div key={idx}>
+                  <a
+                    href={follower.html_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={follower.avatar_url}
+                      alt={`${follower.login}'s avatar`}
+                    />
+                  </a>
+                  <p>{follower.login}</p>
                 </div>
+              ))}
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default GitCard;
 
@@ -28,7 +60,6 @@ export default GitCard;
 
 // this.props.profileUrl passed to App.js in return  avatarUrl={this.state.user.avatar_url}
 // this.props.avatarUrl passed to App.js in return profileUrl={this.state.user.html_url}
-
 
 // https://api.github.com/users/Lfritze - this in browser displays the data
 
