2edcbd1d9678f2973ccdb523a11258babc5c6f6a
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-03 16:12:52 -0500

    avatar renders as a link - added notes on passing props

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios'
+import GitCard from './components/GitCard'
 import './App.css';
 
 class App extends React.Component {
@@ -18,15 +19,30 @@ class App extends React.Component {
 
   fetchApiData = userName => {
     axios
-      .get(`https://api.github.com/users/${userName}`)
+      .get(`https://api.github.com/users/Lfritze`)
       .then(res => {
-        this.setState({ user: res.data})
+        this.setState({ user: res.data});
+
+        axios.get(res.data.followers.followers_url)
+        .then(followerInfo => {
+          this.setState({ followers: followerInfo.data });
+        })
+        .catch(followError => {
+          console.log('error in follower fetch in App.js', followError);
+        });
+      })
+      .catch(err => {
+        console.log('error in request in App.js', err)
       })
   }
 
   render() {
   return (
     <div className="App">
+      <GitCard 
+         avatarUrl={this.state.user.avatar_url}
+         profileUrl={this.state.user.html_url}
+      />
       
     </div>
   );
@@ -34,3 +50,7 @@ class App extends React.Component {
 }
 
 export default App;
+
+// NOTES **********
+
+// https://api.github.com/users/Lfritze - this in browser displays the data
--- github-cards/src/components/GitCard.js
+++ github-cards/src/components/GitCard.js
@@ -1,3 +1,40 @@
 import React from 'react'
+import GithubCalendar from 'github-calendar'
+
+class GitCard extends React.Component {
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.username !== this.props.username) {
+            GithubCalendar('.calendar', this.props.username, { responsive: true})
+            //previousProps go with componentDidUpdate
+            // UserName is a parameter in FetchData in App.js
+        }
+    }
+    render() {
+        return (
+            <div className='git-card-container'>
+                <div className='user'>
+                    <a  href={this.props.profileUrl} target='_blank' rel='noopener noreferrer'> 
+                        <img src={this.props.avatarUrl} alt='avatar name' />
+                    </a>
+                </div>
+            </div>
+        )
+    }
+}
+export default GitCard;
+
+// NOTES ****
+
+// this.props.profileUrl passed to App.js in return  avatarUrl={this.state.user.avatar_url}
+// this.props.avatarUrl passed to App.js in return profileUrl={this.state.user.html_url}
+
+
+// https://api.github.com/users/Lfritze - this in browser displays the data
+
+// rel="noopener noreferrer" should be added to links containing target="_blank" as a precaution against reverse tabnabbing. For more information, please refer to the following article:
+// https://www.jitbit.com/alexblog/256-targetblank---the-most-underestimated-vulnerability-ever/
+
+// When to use target='_blank'   https://css-tricks.com/use-target_blank/
 
 // Github Calendar https://github.com/IonicaBizau/github-calendar#:~:targetText=GitHub%20is%20home%20to%20over,Embed%20your%20GitHub%20calendar%20everywhere.
