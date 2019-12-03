8b19842395f0cbca560043ef48ff3d6346fff8fa
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-03 11:15:31 -0500

    setup api fetch in App.js and componentDidMount installed styled-components and Github calendar

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      user: {},
+      followers: []
+    }
+  }
+
+    componentDidMount() {
+      this.fetchApiData()
+    
+  }
+
+  fetchApiData = userName => {
+    axios
+      .get(`https://api.github.com/users/${userName}`)
+      .then(res => {
+        this.setState({ user: res.data})
+      })
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
+}
 }
 
 export default App;
---
+++ github-cards/src/components/GitCard.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+// Github Calendar https://github.com/IonicaBizau/github-calendar#:~:targetText=GitHub%20is%20home%20to%20over,Embed%20your%20GitHub%20calendar%20everywhere.
