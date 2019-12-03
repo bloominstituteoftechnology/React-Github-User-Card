56dd17aae0ab7d07058904b09342c08e35516e69
wais <waisalmakaleh@gmail.com>
2019-12-03 18:32:35 -0500

    geting code 403 not sure if i maxed out my api

--- githhub-cards/src/App.js
+++ githhub-cards/src/App.js
@@ -6,25 +6,38 @@ class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      followers: []
+      followers: [],
+      followersInfo: []
     }
   }
   componentDidMount(){
+
     axios.get('https://api.github.com/users/Wais-A/followers')
     .then(res => {
       console.log(res.data)
       this.setState({followers: res.data})
     })
-  }
+
+    // this.state.followers.map((item) =>{
+    //  return axios.get(`https://api.github.com/users/${item.login}/followers_url`)
+    //   .then(res=> {
+    //     console.log(res.data)
+    //     this.setState({followersInfo: res.data})
+    //   })
+    //   .catch(err => console.log(err))
+    // })
+    }
+    
 
   render(){
     return(
       <div className='app'>
         {this.state.followers.map((item, key)=>{
           return(
-            <div className='followerInfo' key={key} alt={item.login}>
-              <img src={item.avatar_url}/>
+            <div className='followerInfo' key={key} >
+              <img src={item.avatar_url} alt={item.login}/>
               <h1>{item.login}</h1>
+              
             </div>)
         })}
       </div>
