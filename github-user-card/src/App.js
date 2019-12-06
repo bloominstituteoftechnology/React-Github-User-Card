ccbe05e5603612e815926b9ed007b5c91fae2831
Trey McGarity <trey.mcgarity99@gmail.com>
2019-12-05 23:10:03 -0600

    MVP for single user

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      user: 'TreyMcGarity'
+    }
+  }
+  
+  componentDidMount() {
+    this.getUsers();
+  }
+
+  getUsers = () => {
+      axios.get(`https://api.github.com/users/${this.state.user}`)
+      .then(response => {
+        console.log(response.data);
+        this.setState({
+          user: response.data
+        })
+      })
+      .catch(error => {
+        console.log('Data was not returned', error)
+      })
+  }
+
   render() {
     return(
-      <div>
+      <div className="main-content">
         <h1>App</h1>
+        <div className="user-content">
+          <h3>{this.state.user.name}</h3>
+          <p>{this.state.user.bio}</p>
+          <img className="user-img" src={this.state.user.avatar_url} alt="user's avatar"/>
+        </div>
       </div>
     )
   }
