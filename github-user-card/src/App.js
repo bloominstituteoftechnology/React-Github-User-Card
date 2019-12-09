e0d3301cb7f775012bb8a6cc293df6908fa0d5d7
Trey McGarity <trey.mcgarity99@gmail.com>
2019-12-09 00:25:48 -0600

    working on followers

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -5,40 +5,57 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      user: 'TreyMcGarity'
+    this.state = { 
+      user: 'TreyMcGarity', 
+      followers: '',
+      //jbrock1981
     }
   }
-  
+
   componentDidMount() {
     this.getUsers();
+    this.getFollowers();
   }
 
   getUsers = () => {
       axios.get(`https://api.github.com/users/${this.state.user}`)
       .then(response => {
-        console.log(response.data);
-        this.setState({
-          user: response.data
-        })
+        console.log("data from user axios",response.data);
+        this.setState({ user: response.data })
       })
       .catch(error => {
         console.log('Data was not returned', error)
       })
   }
 
+  getFollowers = () => {
+    axios.get(`https://api.github.com/users/${this.state.user}/following`)
+    .then(response => {
+      console.log("data from followers axios", response.data);
+      this.setState({ followers: response.data })
+    })
+    .catch(error => {
+      console.log('Data was not returned', error)
+    })
+  }
+
   render() {
+    // console.log("user from render:",this.state.user);
+    // console.log("followers from render:",this.state.followers);
     return(
-      <div className="main-content">
-        <h1>App</h1>
+      <div>
+        <h1>Github User Card</h1>
         <div className="user-content">
-          <h3>{this.state.user.name}</h3>
-          <p>{this.state.user.bio}</p>
-          <img className="user-img" src={this.state.user.avatar_url} alt="user's avatar"/>
+            <h3 className="user-name">{this.state.user.name}</h3>
+            <img className="user-img" src={this.state.user.avatar_url} alt="user's avatar"/>
+            <div>
+            <p>Bio:{this.state.user.bio}</p>
+            <p>Location:{this.state.user.location}</p>
+            <p>Followers:{this.state.user.following}</p>
+            </div>
         </div>
       </div>
     )
   }
 }
-
 export default App;
