d709a2f2c9942c000db5f752fb69083e2045c7fe
Heather Nye <heatherdnye@gmail.com>
2019-12-08 19:12:35 -0600

    lifecycles implemented

--- my/src/App.js
+++ my/src/App.js
@@ -6,51 +6,60 @@ class App extends React.Component {
     super()
       this.state = {
         users: 'users',
-        followers: 'followers'
+        followers: []
       }
   }
 
   componentDidMount() {
-    axios.get(`https://api.github.com/users/hdnye`)
-      .then(res => {
-        console.log(res);
+    this.fetchFollowers()
+  };
+
+
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.followers !== this.state.followers) {
+      this.setState({
+       followers: []
+    })    
+     this.fetchFollowers()
+  };
+};
+
+  fetchFollowers = () => {
+    axios.get(`https://api.github.com/${this.state.users}/hdnye/followers`)
+    .then(res => {
+      this.setState({
+       followers: res.data.message
       })
-      .catch(err => {
-        console.log('error:', err)
-      })
+      console.log(res);
+    })
+    .catch(err => {
+      console.log('error:', err)
+    })
   }
 
   handleChange = (event) => {
     this.setState({
-      users: event.target.value,
-      followers: event.targe.value
+      users: event.target.value
     })
   }
 
 
   render() {
     return (
-      <>
+      <div>
       <h1>GitHub User Card</h1>
-        <select value={this.state.user} onChange={this.handleChange}>
+        <select value={this.state.users} onChange={this.handleChange}>
           <option value='users'>User</option>
           <option value='followers'>Followers</option>        
-        </select>
-      </>
+           </select>
+          <div>
+            {this.state.followers && this.state.followers.map((follower, index) => (
+              <img key={index} src={follower} alt='Follower' />
+            ))}
+         </div>
+       </div>   
     )
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default App;
