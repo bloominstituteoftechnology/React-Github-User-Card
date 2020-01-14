96fb083279a3c2d4dd9f065d3d5a660e7c73f835
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-14 18:12:23 -0500

    fetched the user's followers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,69 @@
 import React from 'react';
-import User from './User';
-
 import './App.css';
 
-function App () {
-    return (
-      <div className="App">
-        {/* <h1>My GitUserCard</h1> */}
-        <User />
+import axios from 'axios';
+import UserCard from './UserCard';
+
+class App extends React.Component {
+    // constructor, super, etc, are all still built into this class, just under the hood
+    state = {
+      user: '',
+      followers: []
+    };
+  
+    componentDidMount() {
+      axios
+      .get('https://api.github.com/users/scurtis6')
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          user: res.data
+        });
+      })
+      .catch(err => console.log(err));
+
+      axios
+      .get('https://api.github.com/users/scurtis6/followers')
+      .then(res => {
+        console.log(res.data)
+        this.setState({
+          followers: res.data
+        });
+      })
+      .catch(err => console.log(err));
+    }
+  
+    
+    render() {
+      console.log('App is rendering');
+      return (
+        <div className="user">
+          <div>
+            <UserCard 
+            avatar={this.state.user.avatar_url}
+            name={this.state.user.name}
+            email={this.state.user.email}
+            login={this.state.user.login}
+            html={this.state.user.html_url}
+            blog={this.state.user.blog}
+             />
+          </div>
+
+              {this.state.followers.map(follower => {
+                return (
+                  <div className="followers">
+                    <h2>My followers</h2>
+                    <img src={follower.avatar_url} />
+                    <p>Username: {follower.login}</p>
+                    <p>Website: {follower.html_url}</p>
+                    <p>Followers: {follower.followers_url}</p>
+                    {/* <p>Following: {follower.following_url}</p> */}
+                </div>
+              )
+            })}
       </div>
     );
   }
+}
 
 export default App;
--- my-app/src/UserCard.js
+++ my-app/src/UserCard.js
@@ -3,13 +3,16 @@ import React from 'react';
 function UserCard(props){
     console.log(props)
     return (
-        <div key={props.id}>
-            <h1>{props.name}</h1>
-            <img src={props.avatar} />
-            <p>Username: {props.login}</p>
-            <p>Email: {props.email}</p>
-            <p>Website: {props.html}</p>
-            <p>Blog: {props.blog}</p>
+        <div className='main-container'>
+            <div key={props.id} className="my-info">
+                <h2>{props.name}</h2>
+                <img src={props.avatar} />
+                <p>Username: {props.login}</p>
+                <p>Email: {props.email}</p>
+                <p>Website: {props.html}</p>
+                <p>Blog: {props.blog}</p>
+            </div>
+            <div className='user-info'></div>
         </div>
     )
 }
