bd874b103edb1e333b937daed16ff870f7b1ab12
qb1968 <you@example.com>
2020-01-14 16:35:25 -0500

    Work in progress

---
+++ github-usercard/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import  User from './components/User';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      <h1>Welcome to my World!</h1>
+      </header>
+
+      <User/>
+    </div>
+  );
+}
+
+export default App;
---
+++ github-usercard/src/components/UserCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+function UserCard(props){
+
+
+
+    return(
+
+        <div key ={props.id} className="userClass" >
+            <h2>{props.login}</h2>
+
+            <p><a href={props.html_url}>Link</a></p>
+
+            <img  src={props.avatar_url}/>
+        </div>
+    )
+}
+export default UserCard 
