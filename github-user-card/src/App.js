27112de18574a13116a9b8a547db79ca48908b9c
Christina Kaliora <develop@pirron.one>
2020-01-15 13:49:23 +0100

    project set up

---
+++ github-user-card/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        This is my App!
+      </div>
+    );
+  }
+}
+
+export default App;
