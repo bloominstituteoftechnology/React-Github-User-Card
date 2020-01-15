e3beae8d9d18b4233ec0d4ddd7361e5eb28f9946
nicbongo <nicbongo@protonmail.com>
2020-01-14 21:48:41 -0400

    images linked

--- github-usercard/src/components/FollowerCard.js
+++ github-usercard/src/components/FollowerCard.js
@@ -4,12 +4,14 @@ const FollowerCard = ({ follower }) => {
   const {
     login,
     name,
-    avatar_url
+    avatar_url,
+    html_url
+
   } = follower
 
   return (
     <div style={{width: "1fr", border: "1px solid black", backgroundColor: "white"}}>   
-      <img style={{width: "100%"}} src={avatar_url} alt="profile pic" />
+      <a href={html_url} target="__blank"><img style={{width: "100%"}} src={avatar_url} alt="profile pic" /></a>
       <div>
       <h2>{login}</h2> 
       <h4>{name}</h4>
