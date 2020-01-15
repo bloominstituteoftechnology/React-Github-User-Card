ed326b67e81373dba1e584de8f4ab6823d679160
Aaron Andersen <anderseana@tutanota.com>
2020-01-14 19:19:44 -0600

    installed dependencies and created the appjs page

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import UserPage from './components/UserPage';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  constructor() { 
+    super()
+    this.state = {
+      followersArray: []
+    }
+  }
+  componentDidMount() {
+    const URL = 'https://api.github.com/users/anders529';
+    axios
+      .get(`${URL}`)
+      .then(response => {
+      this.setState({followersArray: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+  render() {
+    return  (
+     <>
+        <UserPage data={this.state.followersArray}/>
+     </> 
+    );
+  }}
 export default App;
