43c6a5ac9495d8693f77fa7a4872d61a2b3d6269
Christina Kaliora <develop@pirron.one>
2020-01-15 18:47:59 +0100

    reactstrao works

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import FollowersData from './FollowersData.js';
+import {
+    Card, Button, CardTitle, CardText, Row, Col
+} from 'reactstrap';
 
-function UserCard(props) {
+const UserCard = (props) => {
     return (
-        <div>
-            <h1>name: {props.name}</h1>
-            <h2>username: {props.login}</h2>
-            <h2>location: {props.location}</h2>
-            <h2>followers: {props.followers}</h2>
-            <img src={props.img}></img>
-            {props.followersData.map(follower => (
-                // <h3>{follower.followersUsename}</h3>
-                <FollowersData img={follower.avatar_url} username={follower.login}/>
-            ))};
-        </div>
+        <Row>
+            <Col sm="6">
+            <Card body>
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Go somewhere</Button>
+            </Card>
+            </Col>
+            <Col sm="6">
+            <Card body>
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Go somewhere</Button>
+            </Card>
+            </Col>
+        </Row>
     )
-}
+};
 
 export default UserCard;
+
+
+
+
+// import React from 'react';
+// import FollowersData from './FollowersData.js';
+
+// function UserCard(props) {
+//     return (
+//         <div>
+//             <h1>name: {props.name}</h1>
+//             <h2>username: {props.login}</h2>
+//             <h2>location: {props.location}</h2>
+//             <h2>followers: {props.followers}</h2>
+//             <img src={props.img}></img>
+//             {props.followersData.map(follower => (
+//                 // <h3>{follower.followersUsename}</h3>
+//                 <FollowersData img={follower.avatar_url} username={follower.login}/>
+//             ))};
+//         </div>
+//     )
+// }
+
+// export default UserCard;
