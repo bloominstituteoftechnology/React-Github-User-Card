d9b6829c67b247b33fae016d7f7cb8ce2d9a0dc3
qb1968 <you@example.com>
2020-01-14 22:16:37 -0500

    Added Icons

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import  User from './components/User';
 import './App.css';
+import {FaGithub} from 'react-icons/fa';
+
 
 function App() {
   return (
     <div className="App">
       <header>
-      <h1>Welcome to my World!</h1>
+     <h1><FaGithub/>Welcome to my world of Github!<FaGithub/></h1>
       </header>
 
       <User/>
--- github-usercard/src/components/UserCard.js
+++ github-usercard/src/components/UserCard.js
@@ -10,7 +10,7 @@ function UserCard(props){
         <div key ={props.id} className="userClass" >
             <h2>{props.login}</h2>
 
-            <p><a href={props.html_url}>Link</a></p>
+            <p><a href={props.html_url}>See What I Can Do</a></p>
 
             <img  src={props.avatar_url}/>
         </div>
