0265ab6215d1d6d83ebacc8fb81b085233a5973c
Ben Koehler <bkoehler85282@gmail.com>
2020-01-17 09:31:47 -0700

    got followers to show up with axios

--- src/App.js
+++ src/App.js
@@ -1,42 +1,64 @@
 import React from "react";
-import UserCard from "./components/UserCard";
+import axios from "axios";
 import "./App.css";
+import { Button } from "reactstrap";
+import UserList from "./components/UserList.js";
 
 class App extends React.Component {
   state = {
     user: [],
-    followers: []
+    followers: [],
+    userSearch: ""
   };
 
-  componentDidMount() {
-    fetch("https://api.github.com/users/bkoehler2016")
-      .then(response => {
-        this.setState({
-          user: response.data
-        });
-        console.log(response);
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  componentDidUpdate() {
+    axios
+      .get(`https://api.github.com/users/bkoehler2016`)
+      .then(res => {
+        console.log(res.data);
+        this.setState({ user: res.data });
+        console.log(this.state);
       })
-      .catch(error => console.log(error))
-      .finally(
-        fetch(`https://api.github.com/users/bkoehler2016/followers`)
-          .then(res => {
-            console.log(res);
-            this.setState({
-              followers: res.data
-            });
-          })
-          .catch(err => {
-            console.log(err);
-          })
-          .finally(console.log("Fetch Complete"))
-      );
+      .catch(error => console.log("Server Error"));
   }
+
+  findUser = event => {
+    event.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.userSearch}/followers`)
+      .then(res => {
+        console.log(res);
+        this.setState({ followers: res.data });
+        console.log(this.state);
+      });
+  };
 
   render() {
     return (
-      <div className="App">
-        <h1>Git Hub Card</h1>
-        <section className="card-container"></section>
+      <div className="container">
+        <div className="search-bar">
+          <input
+            type="text"
+            value={this.state.userSearch}
+            onChange={this.handleChange}
+            name="userSearch"
+          />
+          <Button color="primary" onClick={this.findUser}>
+            Find User
+          </Button>
+        </div>
+        <div className="user-card">
+          <img src={this.state.user.avatar_url} />
+          <h2>{this.state.user.login}</h2>
+          <a href={this.state.user.html_url}> Profile </a>
+        </div>
+        <UserList followers={this.state.followers} />
       </div>
     );
   }
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,14 +1,17 @@
 import React from "react";
 
 class UserCard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
-      <article className="card">
-        <a href={this.props.user.html_url}>
-          <img src={this.props.user.avatar_url} alt="avatar" />
-          <p>{this.props.user.login}</p>
-        </a>
-      </article>
+      <div className="user-card">
+        <img src={this.props.follower.avatar_url} />
+        <h3>{this.props.follower.login}</h3>
+        <a href={this.props.follower.html_url}> Profile </a>
+      </div>
     );
   }
 }
