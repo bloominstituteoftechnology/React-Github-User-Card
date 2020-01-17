d901fe27951609db9ca61fb8f7c54e95c55389c1
Ben Koehler <bkoehler85282@gmail.com>
2020-01-17 08:53:06 -0700

    setup fetch and usercard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from "react";
-
+import UserCard from "./components/UserCard";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Git Hub Card</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    user: [],
+    followers: []
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/bkoehler2016")
+      .then(response => {
+        this.setState({
+          user: response.data
+        });
+        console.log(response);
+      })
+      .catch(error => console.log(error))
+      .finally(
+        fetch(`https://api.github.com/users/bkoehler2016/followers`)
+          .then(res => {
+            console.log(res);
+            this.setState({
+              followers: res.data
+            });
+          })
+          .catch(err => {
+            console.log(err);
+          })
+          .finally(console.log("Fetch Complete"))
+      );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Git Hub Card</h1>
+        <section className="card-container"></section>
+      </div>
+    );
+  }
 }
 
 export default App;
