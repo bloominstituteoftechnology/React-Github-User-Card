9c36e98c83268aadc19dc4c2f4cf005220f97c3b
Ben Koehler <bkoehler85282@gmail.com>
2020-01-17 17:28:00 -0700

    got stretch goal with search working

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import "./App.css";
 import axios from "axios";
 import UserCard from "./components/UserCard";
 import FollowerCard from "./components/FollowersCard";
+import { Button } from "reactstrap";
 
 class App extends React.Component {
   state = {
     user: [],
-    followers: []
+    followers: [],
+    userSearch: ""
+  };
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
   };
 
   componentDidMount() {
@@ -29,10 +37,28 @@ class App extends React.Component {
       .catch(err => console.log(err));
   }
 
+  findUser = event => {
+    event.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.userSearch}/followers`)
+      .then(res => {
+        console.log(res);
+        this.setState({ followers: res.data });
+        console.log(this.state);
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>GitHub User</h1>
+        <input
+          type="text"
+          value={this.state.userSearch}
+          onChange={this.handleChange}
+          name="userSearch"
+        />
+        <Button onClick={this.findUser}>Find User</Button>
         {this.state.user.map(item => (
           <UserCard
             key={item.id}
