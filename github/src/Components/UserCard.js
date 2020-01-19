c898bb7ce8c516e6e2aff313e96cd0738f18e682
Veto Ramirez <vetorobert3@gmail.com>
2020-01-18 16:03:21 -0800

    Wokring on styled-components. Need to override the original css.

--- github/src/Components/UserCard.js
+++ github/src/Components/UserCard.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledH3 = styled.h3`
+  display: flex;
+
+  font-size: 24px;
+  align-items: flex-end;
+`;
 
 const UserCard = props => {
   console.log("vr: usercard: props: ", props)
   return (
     <div>
-      <h3>{props.users.login}</h3>
+      <StyledH3>{props.users.login}</StyledH3>
       <img src={props.users.avatar_url} alt="followerImage" />
     </div>
   )         
