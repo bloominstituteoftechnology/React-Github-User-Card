f546d709adabc87aed3cde670af71048a15d9546
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2020-01-19 17:18:10 -0800

    initial commit

--- github-react-usercard/src/App.js
+++ github-react-usercard/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-    <h2>finally working</h2>
+    <h2>Hello world</h2>
       </header>
     </div>
   );
