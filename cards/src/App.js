cf4ffaaada927574dfc23a06805fc38c63c42df5
Kayla Dailey <kayladailey000@gmail.com>
2020-01-20 23:50:52 -0600

    setup CDM for user

--- cards/src/App.js
+++ cards/src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from "react";
 
 
-function App() {
-  return (
-    <div className="App">
-  
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount(){
+   
+    const getFriends = () => {
+      fetch('https://api.github.com/users/')
+        .then(response => response.json())
+        .then(friends => this.setState({friends: friends}))
+        .catch(err => console.log('Error'));
+  };
+
+    this.setState({});
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1> Welcome to Friends! </h1>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
