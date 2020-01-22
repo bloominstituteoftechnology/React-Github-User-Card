6dc9d50ef29c9a3a9bc4cf76437e65e125303d7d
Aeson Johnson <aeson.m.johnson@gmail.com>
2020-01-21 18:23:33 -0800

    mvp technically met

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import UserCard from "./components/UserCard"
+import User from "./components/User"
 import logo from './logo.svg';
 import './App.css';
 
 export default class App extends React.Component {
-
   render(){
     return (
       <div className="App">
         <h1>My Followers</h1>
-        <UserCard />
+        <User />
       </div>
     );
   }
--- github-usercard/src/components/UserCard.js
+++ github-usercard/src/components/UserCard.js
@@ -1,63 +1,12 @@
 import React from "react";
-import axios from "axios";
-import UserCard from "./UserCard";
 
-export default class User extends React.Component{
-    constructor() {
-        super();
-        this.state = {
-            user: {},
-            followers: []
-        }
-    }
-
-    componentDidMount() {
-        axios.get("https://api.github.com/users/AesonJohnson")
-        .then(response => {
-            console.log(response)
-            this.setState({
-                user: response.data
-            })
-        })
-        .catch(error => {
-            console.log(error)
-        })
-        
-        axios.get("https://api.github.com/users/AesonJohnson/followers")
-        .then(response => {
-            console.log(response)
-            this.setState({
-                followers: response.data
-            })
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    }
-
-    render() {
-        return(
-            <div>
-                {
-                    <UserCard
-                        avatar_url={this.state.user.avatar_url}
-                        id={this.state.user.id}
-                        name={this.state.user.name}
-                        login={this.state.user.login}
-                        html_url={this.state.user.html_url}
-                    />
-                }
-
-                {this.state.followers.map(follower =>
-                    <UserCard
-                        avatar_url={follower.avatar_url}
-                        id={follower.id}
-                        name={follower.name}
-                        login={follower.login}
-                        html_url={follower.html_url}
-                    />
-                )}
-            </div>
-        )
-    }
+export default function UserCard(props) {
+    return(
+        <div>
+            <img src={props.avatar_url}/>
+            {props.name}
+            {props.login}
+            {props.html_url}
+        </div>
+    );
 }
