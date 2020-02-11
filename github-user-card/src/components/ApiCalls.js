de2dc003468aa03aa20db430e93aab3d6feb9436
miriam-sullivan <maljayd42@gmail.com>
2020-02-11 16:07:41 -0600

    added user data

--- github-user-card/src/components/ApiCalls.js
+++ github-user-card/src/components/ApiCalls.js
@@ -2,14 +2,11 @@ import React from "react";
 import axios from "axios";
 import UserCard from "./UserCard";
 class ApiCalls extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      UserData: [],
-      FollowersData: [],
-      error: ""
-    };
-  }
+  state = {
+    UserData: [],
+    FollowersData: [],
+    error: ""
+  };
 
   componentDidMount() {
     axios
@@ -18,27 +15,18 @@ class ApiCalls extends React.Component {
         this.setState({
           UserData: res.data
         });
+        console.log(res.data);
       })
       .catch(err => console.log("the error for the git for usercard", err));
   }
+
   render() {
     return (
       <div className="container">
         <h1>My React Followers and I:</h1>
-        {this.state.UserData.map(data => {
-          <UserCard>
-            <h2>{data.name}</h2>
-            <h3>{data.login}</h3>
-            <p>followers: {data.followers}</p>
-            <p>following: {data.following}</p>
-            <p>bio: {data.bio}</p>
-            <small>created: {data.created_at}</small>
-          </UserCard>;
-        })}
-        ;
+        <UserCard UserData={this.state.UserData} />
       </div>
     );
   }
 }
-
 export default ApiCalls;
--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -3,12 +3,13 @@ import React from "react";
 const UserCard = props => {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <h3>{props.login}</h3>
-      <p>followers: {props.followers}</p>
-      <p>following: {props.following}</p>
-      <p>bio: {props.bio}</p>
-      <small>created: {props.created_at}</small>
+      <img src={props.UserData.avatar_url} />
+      <h2>{props.UserData.name}</h2>
+      <h3>{props.UserData.login}</h3>
+      <p>followers: {props.UserData.followers}</p>
+      <p>following: {props.UserData.following}</p>
+      <p>bio: {props.UserData.bio}</p>
+      <small>created: {props.UserData.created_at}</small>
     </div>
   );
 };
