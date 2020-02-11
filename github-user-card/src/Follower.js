a0b2d1bc30c6fa5d4926ede2cda7dd4ac1390615
hlee2542 <ou40a9rtbp@gmail.com>
2020-02-11 15:12:27 -0800

    updated follower card

--- github-user-card/src/Follower.js
+++ github-user-card/src/Follower.js
@@ -4,12 +4,28 @@ import './App.css';
 class Follower extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            followers: '?',
+            following: '?',
+            location: '?',
+            bio: '?'
+        }
+    }
+    componentDidMount() {
+        fetch(this.props.follower.url)
+            .then(res => res.json())
+                .then(data => this.setState({followers: data.followers, following: data.following, location: data.location, bio: data.bio}))
     }
     render() {
         return (
             <div className='follower-container'>
                 <div><img src={this.props.follower.avatar_url}/></div>
                 <p>Username: {this.props.follower.login}</p>
+                <p>Profile: <a href={this.props.follower.url}>{this.props.follower.url}</a></p>
+                <p>Followers: {this.state.followers}</p>
+                <p>Following: {this.state.following}</p>
+                <p>Location: {this.state.location ? this.state.location : 'None'}</p>
+                <p>Bio: {this.state.bio ? this.state.bio : 'None'}</p>
             </div>
         );
     }
