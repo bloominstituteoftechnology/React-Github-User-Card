182b635c14c8bded468b851f1df9666146a00d13
Mandi Haase <haase1020@gmail.com>
2020-02-11 20:30:23 -0500

    add to UserCard component and put basic styled component portions into component

--- github-card/src/components/UserCard.js
+++ github-card/src/components/UserCard.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 
+
+const UserDiv = styled.div `
+
+`;
+
+const UserImage = styled.image`
+
+`;
+
 function UserCard (props){
 
     return(
-        <Div>
+        <div>
+            <div>
             <h1>User Card</h1>
-        </Div>
+            {props.userData.map(data => (
+                <UserDiv key={data.id}>
+                    <UserImage src = {data.avatars_url} alt= {data.name} />
+                    <h2>Name: {data.name}</h2>
+                    <h2>About: {data.bio}</h2>
+                </UserDiv>
+            ))}
+            </div>
+            <h1>{props.user} Those Who are Following</h1>
+            <div>
+                {props.followers.map(follower => (
+                    <div key={follower.id}>
+                        <UserImage src={follower.avatar_url}} alt = {follower.name} />
+                        <h2> Name: {follower.name}</h2>
+                        <h2> About: {follower.bio}</h2>
+                    </div>    
+                ))}
+            </div>
+        </div>
     );
 }
 
