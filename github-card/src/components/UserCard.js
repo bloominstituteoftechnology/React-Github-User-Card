cc2a9ecf82c867476d8091fa4dd7730dc8b72d35
Mandi Haase <haase1020@gmail.com>
2020-02-11 20:12:47 -0500

    add UserCard component and App.js information

---
+++ github-card/src/components/UserCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function UserCard (props){
+
+    return(
+        <Div>
+            <h1>User Card</h1>
+        </Div>
+    );
+}
+
+export default UserCard;
