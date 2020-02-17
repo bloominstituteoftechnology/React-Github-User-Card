3f4e7de09a5d0814632183cc0f76126c3c1ef13c
Antonio Dinkins <afdinki@gmail.com>
2020-02-17 10:31:34 -0500

    Updated UserProfile.js

---
+++ github-user-card/src/components/UserProfile.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledLi = styled.li`
+  list-style-type: none;
+`
+const StyledUl = styled.ul`
+  display: flex;
+  flex-direction: column;
+`
+const StyledDiv = styled.div`
+  width: 75%;
+  padding: 2%;
+  margin: 0 auto;
+  background: powderblue;
+  border: 1 solid black;
+  box-shadow: 5px 7px 5px #888888;
+
+`
+const StyledImg = styled.img`
+  width: 40%;
+`
+const UserProfile = ({user}) => {
+  console.log(user)
+  const userCompany = user.company === null ? "None": user.company
+  const userEmail = user.email === null ? "Not Provided": user.email
+  const userBio = user.bio === null ? "Not Provided": user.bio
+
+  return(
+    <StyledDiv>
+      <h1>{user.name}</h1>
+      <StyledImg 
+        src={user.avatar_url} alt={user.name}
+      /> <br/>
+      <StyledUl>
+        <StyledLi>Company: {userCompany}</StyledLi> <br/>
+        <StyledLi>Location: {user.location}</StyledLi> <br/>
+        <StyledLi>Email: {userEmail}</StyledLi><br/>
+        <StyledLi>Bio: {userBio}</StyledLi><br/>
+        <StyledLi>Followers: {user.followers} </StyledLi><br/>
+        <StyledLi>Following: {user.following} </StyledLi>
+
+      </StyledUl>
+
+
+     </StyledDiv>
+  )
+}
+
+export default UserProfile;
