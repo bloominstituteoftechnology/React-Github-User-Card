167f35242bd127dcedb493b35446790e99b0e70b
Hutch Crowley <tothecoda@gmail.com>
2020-02-18 08:49:35 -0600

    init commit - fixed repo MVP + styles

---
+++ src/components/Follower.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Card from 'react-bootstrap/Card'
+
+const Follower = props => {
+  return (
+    <>
+      <Card className='bg-secondary' id='follower-card'>
+        <Card.Img
+          variant='bottom'
+          src={props.picture}
+          className='follower-image'
+        />
+        <Card.ImgOverlay>
+          <Card.Text>
+            <div className='handle'>
+              <h4>GitHub User Name:</h4>
+              <p>{props.handle}</p>
+            </div>
+            <div className='profile'>
+              <h4>Profile:</h4>
+              <a href={props.profile}>
+                <h5>{props.profile}</h5>
+              </a>
+            </div>
+          </Card.Text>
+        </Card.ImgOverlay>
+      </Card>
+    </>
+  )
+}
+
+export default Follower
