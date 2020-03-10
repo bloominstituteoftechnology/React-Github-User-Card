b4386d0382db343a82afb87999407d7dcc543859
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2020-03-10 17:51:35 -0600

    Made the axios call for the github followers

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
@@ -8,7 +7,7 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      gitHub: []
+      gitHub: [], followers: []
     };
   }
   componentDidMount() {
@@ -18,12 +17,24 @@ class App extends React.Component {
       console.log(res);
     })
       .catch(err => console.log(err));
+
+      axios.get('https://api.github.com/users/Mitch98k/followers')
+    .then(res => {
+      this.setState({followers: res.data});
+      console.log(res);
+    })
+      .catch(err => console.log(err));
   };
+ 
+    
+  
   render() {
     return (
       <div className="App">
         <h1>My Github</h1>
         {this.state.gitHub}
+        <h1>My Followers</h1>
+        {this.state.followers.map(follower => follower.login)}
       </div>
     );
   }
