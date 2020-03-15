e97b353cb789462b18697e6f393835abe8f439ce
Jason Neale <jason@novus.one>
2020-03-15 11:30:30 -0400

    Updated CRA

--- react-github-user-card/src/index.js
+++ react-github-user-card/src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Package imports
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// File imports
+import './App.css'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Component imports
+import App from './App'
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
