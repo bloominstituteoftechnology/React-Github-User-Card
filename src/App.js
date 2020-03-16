ddd1395214f452e8b0f569b8d13e676ba74246c5
aaronmatson1 <matsonaaron1@gmail.com>
2020-03-15 20:33:48 -0600

    deleted images from project. Just displaying the MVP data.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import Card from './Card';
-import Lambdalogo from './lambda-logo.png';
-import Githublogo from './github-logo.png';
+
 
 class App extends React.Component {
 
@@ -20,7 +19,6 @@ class App extends React.Component {
 componentDidMount() {
   axios
   .get('https://api.github.com/users/aaronmatson1')
-  // .get('https://api.github.com/users')
   .then(res => {
     console.log(res.data)
     this.setState({
@@ -53,17 +51,13 @@ axios
   .catch(err => console.log(err.message));
 }
 
-
-
-
-
       render() {
        return (
          <div className="container">
            <div className="header">
-             <img src={Lambdalogo} alt="Lambda Logo"/>
+
              <p>❤️'s</p>
-             <img src={Githublogo} alt="GitHub Logo" />
+
            </div>
            <div className="cards">
              {this.state.user}
@@ -75,9 +69,5 @@ axios
 
 
   }
-
-
-
-
 
 export default App;
