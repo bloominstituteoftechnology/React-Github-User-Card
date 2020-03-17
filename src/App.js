261c7a4173c6bd01e57415882844834172f9a2ac
aaronmatson1 <matsonaaron1@gmail.com>
2020-03-16 20:45:10 -0600

    Reconfigured some of CSS of the app. MVP met.

--- src/App.js
+++ src/App.js
@@ -1,73 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
-import Card from './Card';
+import React from "react";
+import User from "./components/User";
+import "./App.css";
 
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>GitHub User Card</h1>
+      </header>
 
-class App extends React.Component {
-
-  constructor() {
-    super();
-    this.state={
-      user:[],
-      followers:[]
-    };
-  }
-
-//FETCHING MY USER DATA
-componentDidMount() {
-  axios
-  .get('https://api.github.com/users/aaronmatson1')
-  .then(res => {
-    console.log(res.data)
-    this.setState({
-      user:[
-      res.data.name,
-      res.data.bio,
-      res.data.login,
-      res.data.followers,
-      res.data.following
-    ]
-    });
-  })
-  .catch(err => console.log(err.message));
-
-//FETCHING FOLLOWERS
-axios
-.get('https://api.github.com/users/aaronmatson1')
-.then(res =>{
-  console.log(res.data)
-  this.setState({
-    followers:res.data.forEach(item => {
-      axios.get(item.url)
-      .then(res => {
-        console.log(res.data);
-        return res.data
-      })
-    })
-  });
-})
-  .catch(err => console.log(err.message));
+      <User />
+    </div>
+  );
 }
-
-      render() {
-       return (
-         <div className="container">
-           <div className="header">
-
-             <p>❤️'s</p>
-
-           </div>
-           <div className="cards">
-             {this.state.user}
-             {this.state.followers}
-           </div>
-         </div>
-       );
-     }
-
-
-  }
 
 export default App;
