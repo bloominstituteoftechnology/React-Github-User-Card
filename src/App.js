4eb4dbfcc716d09c928e6ad5dcdb2e38353063e5
Erik Scow <erikscow@gmail.com>
2020-03-17 18:22:56 -0700

    Final commit.

--- src/App.js
+++ src/App.js
@@ -9,25 +9,37 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      userData: []
+      userData: [],
+      followerData: []
     }
   }
 
   componentDidMount() {
     axios.get('https://api.github.com/users/erikscow')
       .then(res => {
-        console.log(res)
         this.setState({
           userData: res.data
         })
       })
       .catch(err => console.log('Something went wrong in the API call: ', err))
+      axios.get('https://api.github.com/users/erikscow/followers')
+        .then(res => {
+          console.log(res)
+          this.setState({
+            followerData: res.data
+          })
+        })
+        .catch(err => console.log('follower api call error:', err))
   }
 
   render(){
     return (
       <div className='App'>
+        <h1>React Github User Card</h1>
         <UserCard userData={this.state.userData} />
+        {this.state.followerData.map((item, i) => {
+          return(<UserCard key={i} userData={this.state.followerData[i]}/>)
+        })}
       </div>
     )
   }
