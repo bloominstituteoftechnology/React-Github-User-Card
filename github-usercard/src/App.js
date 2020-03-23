be3a0a68fd919bf9d88bb6c0a04276f9faf0fd1b
yirano <yirano@protonmail.ch>
2020-03-22 20:24:35 -0400

    Set up mount lifecycle method

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import axios from "axios";
 import UserCardContainer from "./Component/UserCardContainer";
 import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: ""
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/yirano")
+      .then(res => {
+        console.log(res);
+        this.setState({ user: res });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   render() {
