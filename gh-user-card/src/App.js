b07dc45b7494875ce06bcbd5e136298435b19779
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-07 17:06:43 -0500

    followers and following are not getting the correct information. I keep getting locked out on the API rate limit.

--- gh-user-card/src/App.js
+++ gh-user-card/src/App.js
@@ -22,10 +22,31 @@ class App extends React.Component {
             });
         axios
             .get('https://api.github.com/users/Perezented/followers')
+            .then((response) => {
+                console.log(response.data);
+                response.data.forEach((value) => {
+                    const theirName = value.login;
+                    axios
+                        .get(`https://api.github.com/users/${theirName}`)
+                        .then((response2) => {
+                            console.log(response2);
+                            // this.setState({
+                            //     followers: response2,
+                            // });
+                        })
+                        .catch((error) => {
+                            console.log('ERRORZ MENG!', error);
+                        });
+                });
+                // this.setState({
+                //       followers: res.data,
+                //   });
+            });
+        axios
+            .get('https://api.github.com/users/Perezented/following')
             .then((res) => {
-                console.log(res.data);
                 this.setState({
-                    followers: res.data,
+                    following: res.data,
                 });
             });
     }
@@ -36,18 +57,55 @@ class App extends React.Component {
                 <h1>Github Card Displayer</h1>
                 <div>
                     <img src={this.state.myCard.avatar_url} />
-                    <h2>{this.state.myCard.login}</h2>
+                    <h2>{this.state.myCard.login} is my Github handle</h2>
+                    <h3>Hey there, I'm {this.state.myCard.name}</h3>
+                    <h5>{this.state.myCard.bio}</h5>
+                    <h3>Location: {this.state.myCard.location}</h3>
+                    <p>
+                        Number of Public Repos: {this.state.myCard.public_repos}
+                    </p>
+                    <p>Followers: {this.state.myCard.followers}</p>
+                    <p>Following: {this.state.myCard.following}</p>
+                    <p>{this.state.myCard.html_url}</p>
                 </div>
-                <section>
-                    <h3>Followers</h3>
-                    {this.state.followers.map((follower) => (
-                        <h4>
-                            {console.log(follower)}
-                            {follower.login}
-                        </h4>
-                    ))}
-                </section>
-                <section></section>
+                <div>
+                    <section>
+                        <h2>Followers</h2>
+                        {this.state.followers.map((follower) => (
+                            <div>
+                                <h2>{follower.login} is my Github handle</h2>
+                                <h3>Hey there, I'm {follower.name}</h3>
+                                <h5>{follower.bio}</h5>
+                                <h3>Location: {follower.location}</h3>
+                                <p>
+                                    Number of Public Repos:{' '}
+                                    {follower.public_repos}
+                                </p>
+                                <p>Followers: {follower.followers}</p>
+                                <p>Following: {follower.following}</p>
+                                <p>{follower.html_url}</p>
+                            </div>
+                        ))}
+                    </section>
+                    <section>
+                        <h2>Following</h2>
+                        {this.state.following.map((following) => (
+                            <div>
+                                <h2>{following.login} is my Github handle</h2>
+                                <h3>Hey there, I'm {following.name}</h3>
+                                <h5>{following.bio}</h5>
+                                <h3>Location: {following.location}</h3>
+                                <p>
+                                    Number of Public Repos:{' '}
+                                    {following.public_repos}
+                                </p>
+                                <p>Followers: {following.followers}</p>
+                                <p>Following: {following.following}</p>
+                                <p>{following.html_url}</p>
+                            </div>
+                        ))}
+                    </section>
+                </div>
             </div>
         );
     }
