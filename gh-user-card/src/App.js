8f41555ec8e582f6806af2587ebc4cfe1a7546c3
Perezented <57721734+Perezented@users.noreply.github.com>
2020-04-07 14:47:10 -0500

    Added an axios link, and have talked to github thru the air!!!!

--- gh-user-card/src/App.js
+++ gh-user-card/src/App.js
@@ -2,8 +2,32 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
-function App() {
-    return <div className="App"></div>;
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            myCard: [],
+        };
+    }
+    componentDidMount() {
+        axios
+            .get('https://api.github.com/users/Perezented')
+            .then((response) => {
+                console.log(response.data);
+                this.setState({
+                    myCard: response.data.message,
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Github Card Displayer</h1>
+                <div></div>
+            </div>
+        );
+    }
 }
 
 export default App;
