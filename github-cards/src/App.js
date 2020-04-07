67800ec7d37de14a32ebfb94d447809fe88bfb81
Webbuildermn <gerald.ryan40@yahoo.com>
2020-04-07 14:56:13 -0500

    got data returned from axios super easy barely an inconvenience

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from "axios"
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component
+{
+  constructor()
+  {
+    super()
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount()
+  {
+    axios.get(`https://api.github.com/users/GeraldRyan`)
+    .then(response=>{
+      this.setState({
+        users: response.data
+      })
+      console.log(response.data)
+    })
+  }
+
+  render()
+  {
+
+
+    return (
+      <div className="App">
+      <h1>Github User Info</h1>
+
+
+      </div>
+    );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ github-cards/src/shapeofData.js
@@ -0,0 +1,37 @@
+response.data shape of data::::
+
+
+
+{login: "GeraldRyan", id: 46059943, node_id: "MDQ6VXNlcjQ2MDU5OTQz", avatar_url: "https://avatars1.githubusercontent.com/u/46059943?v=4", gravatar_id: "", …}
+login: "GeraldRyan"
+id: 46059943
+node_id: "MDQ6VXNlcjQ2MDU5OTQz"
+avatar_url: "https://avatars1.githubusercontent.com/u/46059943?v=4"
+gravatar_id: ""
+url: "https://api.github.com/users/GeraldRyan"
+html_url: "https://github.com/GeraldRyan"
+followers_url: "https://api.github.com/users/GeraldRyan/followers"
+following_url: "https://api.github.com/users/GeraldRyan/following{/other_user}"
+gists_url: "https://api.github.com/users/GeraldRyan/gists{/gist_id}"
+starred_url: "https://api.github.com/users/GeraldRyan/starred{/owner}{/repo}"
+subscriptions_url: "https://api.github.com/users/GeraldRyan/subscriptions"
+organizations_url: "https://api.github.com/users/GeraldRyan/orgs"
+repos_url: "https://api.github.com/users/GeraldRyan/repos"
+events_url: "https://api.github.com/users/GeraldRyan/events{/privacy}"
+received_events_url: "https://api.github.com/users/GeraldRyan/received_events"
+type: "User"
+site_admin: false
+name: "Gerald Ryan"
+company: null
+blog: ""
+location: "Minnesota"
+email: null
+hireable: null
+bio: "Born and raised."
+public_repos: 47
+public_gists: 0
+followers: 16
+following: 8
+created_at: "2018-12-22T00:44:06Z"
+updated_at: "2020-04-07T18:54:50Z"
+__proto__: Object
