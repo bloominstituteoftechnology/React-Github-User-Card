790f6b8eee81db72f734850982029d87d29aefcb
DenisChuvilin <denischuv@gmail.com>
2020-04-07 12:04:29 -0700

    started project

---
+++ react-usercards/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component() {
+  constructor() {
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
+}
+export default App;
