bf9d879fb1c5cb3267fe9c146264736ec9e52d3a
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-04-07 16:23:28 -0700

    Everything is working, but no style has been applied yet

--- src/App.js
+++ src/App.js
@@ -1,31 +1,76 @@
 import React from 'react';
-// import TodoList from './components/TodoList';
-// import TodoForm from './components/TodoForm';
-// import "./components/Todo.css";
+import axios from 'axios';
+import Followers from "./components/Followers";
+import Profile from "./components/Profile";
 
 
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  constructor() {
-    super();
-    this.state = {
-
-    };
+ state = {
+   user: {},
+   followers: []
+ };
+  componentDidMount() {
+    axios.get('https://api.github.com/users/LeesahMasko').then(response => {
+      console.log(response.data, "user response.data");
+      this.setState({ user: response.data });
+    });
+    axios.get('https://api.github.com/users/LeesahMasko/followers').then(response => {
+      console.log(response.data, "followers response.data");
+      this.setState({ followers: response.data});
+    });
   }
 
-
-
   render() {
-    console.log('rendering...');
+    console.log(this.state.user);
+    console.log(this.state.followers);
     return (
-      <div className="App">
-        Howdy
-      </div>
+      <>
+        <div className="app">
+          <h1>GitHub User Cards</h1>
+          <Profile user={this.state.user} />
+          <Followers
+            followers={this.state.followers}
+            key={this.state.followers}
+          />
+        </div>
+      </>
     );
   }
 }
 
 export default App;
+
+/*
+  "login": "LeesahMasko",
+  "id": 60702582,
+  "node_id": "MDQ6VXNlcjYwNzAyNTgy",
+  "avatar_url": "https://avatars2.githubusercontent.com/u/60702582?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/LeesahMasko",
+  "html_url": "https://github.com/LeesahMasko",
+  "followers_url": "https://api.github.com/users/LeesahMasko/followers",
+  "following_url": "https://api.github.com/users/LeesahMasko/following{/other_user}",
+  "gists_url": "https://api.github.com/users/LeesahMasko/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/LeesahMasko/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/LeesahMasko/subscriptions",
+  "organizations_url": "https://api.github.com/users/LeesahMasko/orgs",
+  "repos_url": "https://api.github.com/users/LeesahMasko/repos",
+  "events_url": "https://api.github.com/users/LeesahMasko/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/LeesahMasko/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Lisa Maskovich",
+  "company": "Lambda School",
+  "blog": "",
+  "location": "San Francisco, CA",
+  "email": null,
+  "hireable": null,
+  "bio": "Lisa Maskovich\r\n",
+  "public_repos": 34,
+  "public_gists": 0,
+  "followers": 15,
+  "following": 5,
+  "created_at": "2020-02-05T14:34:38Z",
+  "updated_at": "2020-04-07T18:06:27Z"
+*/
