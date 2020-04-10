61df69aa7f559a19193359a9f3ab0d3c3c8f9368
Ariel Rodriguez <Ariels713@gmail.com>
2020-04-10 22:01:32 +0000

    follower photos are now links

--- my-app/src/components/Followers.js
+++ my-app/src/components/Followers.js
@@ -1,11 +1,20 @@
 import React from "react";
-import { Card, Dimmer, Loader } from "semantic-ui-react";
+import { Card } from "semantic-ui-react";
 
 const Followers = friend => (
   <div>
     <Card.Group itemsPerRow={4}>
-      {friend.friend.map(photo => {
-        return <Card raised image={photo.avatar_url} key={Math.random()} />;
+      {friend.friend.map(user => {
+        return (
+          <Card
+            raised
+            image={user.avatar_url}
+            key={Math.random()}
+            as="a"
+            href={user.html_url}
+            target="blank"
+          />
+        );
       })}
     </Card.Group>
   </div>
