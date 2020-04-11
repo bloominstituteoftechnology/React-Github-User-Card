ef5db2d679be9898cad52e265cfa774c2bd86fea
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-10 17:58:35 -0700

    beginning to style

--- src/components/FollowerList.js
+++ src/components/FollowerList.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const FollowerList = ({follow} )=> {
     return (
         <div>
+            <h1>Followers' Profiles</h1>
             <h3>{follow.login}</h3>
             <img src={follow.avatar_url} alt ="follower profile"/>
             <a href={follow.html_url}>Follower's Repositories</a>
--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
 
 const User = ({user}) => {
     return (
         <div className="User">
             <div>
+                <h1>User's Profile</h1>
                 <h3>{user.login}</h3>
                 <img src={user.avatar_url} alt="user profile"/>
                 <a href={user.html_url}>My Repositories</a>
