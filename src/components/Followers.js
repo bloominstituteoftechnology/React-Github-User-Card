98a08c9c3089da2032e4b61b6933298cabad148f
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-10 17:20:51 -0700

    imported axios into Followers.js

--- src/components/Followers.js
+++ src/components/Followers.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import axios from 'axios';
 
 class Followers extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            followers: []
+        }
+    }
 
     render() {
         return()
