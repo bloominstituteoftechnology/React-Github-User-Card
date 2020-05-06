603bd28480ccd3fa566bb41e91d6252409393001
Eric Taylor <erictaylor@Erics-MacBook-Pro.local>
2020-05-05 22:48:40 -0400

    Rendered my github user data in the UserCard file via props

--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -6,7 +6,12 @@ export default class Usercard extends React.Component{
 
         return(
             <div>
+                <img width="200" src={this.props.userData.avatar_url} />
                 <p>Login: {this.props.userData.login}</p>
+                <p>Name: {this.props.userData.name}</p>
+                <p>Following: {this.props.userData.following}</p>
+                <p>Followers: {this.props.userData.followers}</p>
+                
             </div>
         )
 
