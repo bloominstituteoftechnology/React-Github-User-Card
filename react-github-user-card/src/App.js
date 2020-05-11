58c00a46b56fb07cbd4ba5205ed4a7b9449a5cfd
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-05-11 18:19:48 -0400

    user cards being displayed

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  render() {
+    const loading = true;
+
+ 
+
+    return (
+      <div className='App'>
+       <Navbar /> 
+       <div className="container">
+        <Users />
+       </div>
+       
+      </div>
+    );
+  }
 }
 
 export default App;
