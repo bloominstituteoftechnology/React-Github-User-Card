82fdd6c61385617a243b543bfa068033c635774d
reggieTheCoder <33520187+reggieTheCoder@users.noreply.github.com>
2020-05-11 20:46:48 -0400

    moved state to app component, initiated an axios call, populating display with data from github users

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,21 +1,37 @@
 import React, { Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
+import axios from 'axios'
 
 
 class App extends Component{
+  state = {
+    users: [],
+    // set the initial state of loading to false
+    loading: false
+  };
+
+  async componentDidMount(){
+    //  prior to api call set loading to true
+    this.setState({ loading: true });
+
+    const res = await axios.get('https://api.github.com/users');
+
+    // after the api call populates data set user to equal res data (the information returned from the axios call and loading back to false)
+    this.setState({users: res.data, loading: false});
+  }
+
+
+
+
+
   render() {
-    const loading = true;
-
- 
-
     return (
       <div className='App'>
        <Navbar /> 
        <div className="container">
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
        </div>
        
       </div>
--- react-github-user-card/src/components/users/Users.js
+++ react-github-user-card/src/components/users/Users.js
@@ -2,33 +2,10 @@ import React, {Component} from 'react'
 import UserItem from './UserItem'
 
 class User extends Component {
-    state = {
-        users: [
-            {
-                id: '1',
-                login:"octocat",
-                avatar_url: "https://avatars0.githubusercontent.com/u/583231?s=460&v=4",
-                html_url: "https://github.com/octocat"
-               },
-               {
-                id: '2',
-                login:"octocat",
-                avatar_url: "https://avatars0.githubusercontent.com/u/583231?s=460&v=4",
-                html_url: "https://github.com/octocat"
-               },
-               {
-                id: '3',
-                login:"octocat",
-                avatar_url: "https://avatars0.githubusercontent.com/u/583231?s=460&v=4",
-                html_url: "https://github.com/octocat"
-               },
-        ]
-    }
-
     render() {
         return (
             <div style={userStyle}>
-                {this.state.users.map(user => (
+                {this.props.users.map(user => (
                    <UserItem key={user.id} user={user}/> 
                 ))}
             </div>
@@ -38,7 +15,7 @@ class User extends Component {
 
  const userStyle = {
      display: 'grid', 
-     gridTemplateColumns: 'repeat(3, 1fr)'
+     gridTemplateColumns: 'repeat(3, 1fr)',
  }
     
 
