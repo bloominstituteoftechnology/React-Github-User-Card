8a20c165b701213fd66932784b52736a4f1a08d7
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-06-02 16:41:18 -0500

    changed app function to class component

--- src/src/App.js
+++ src/src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import UserItem from './components/UserItem'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    <h1> Github User built with React</h1>
-    <UserItem />
-    </div>
-  );
-}
+class App extends Component {
+    render () {
+      
+      return (
+      <div className="App">
+      <h1> Github User built with React</h1>
+      <UserItem />
+      </div>
+    );
+  }
+};
 
 export default App;
