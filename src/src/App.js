ebe4d84447c8f1d51c1622c0d33f3cc29755ccdc
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-06-02 22:36:36 -0500

    working on Searchbar

--- src/src/App.js
+++ src/src/App.js
@@ -1,33 +1,54 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+//import User from './components/User';
 import Users from './components/Users';
-
+import Search from './components/Search';
 import './App.css';
 
 class App extends Component {
 
   state = {
-    users: [],
+    users: [], //all users are and array of objects
+    user: {},  //single user is empty object
     loading: false    
   }
   //start lifecycle method
   //call to api using axios
 
-  async componentDidMount() {
-    this.setState({ loading: true });
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
 
-    const res = await axios.get('https://api.github.com/users');
+  //   const res = await axios.get('https://api.github.com/users');
 
-    this.setState({ users: res.data, loading: false });
-  }
+  //   this.setState({ users: res.data, loading: false });
+  // }
+  //Search Github Users
+
+  searchUsers = async text => {
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}`);
+
+    this.setState({ users: res.data.items, loading: false });
+   
+  };
+
+  //get single github User
+getUser = async (username) => {
+  this.setState({ loading: true });
+
+  const res = await axios.get(`https://api.github.com/users/${username}`);
+
+  this.setState({ user: res.data, loading: false });
+}
+
     render () {
-      
+      //const { user, loading } = this.state;
       return (
       <div className="App">
       <h1> Github User built with React</h1>
-      
+      {/* <User getUser={this.getUser} user={user} loading={loading} /> */}
      <div className='container'>
-     <h2> Possibly Search bar here ...?</h2>
+
+     <Search searchUsers={this.searchUsers} />
 
      {/* //pass in loading and users as props */}
        <Users loading={this.state.loading} users={this.state.users} />
---
+++ src/src/components/Search.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export class Search extends Component {
+  //set component level state (only for this component)
+  //initial state of search bar
+  state = {
+    text: ''
+  };
+
+onSubmit = (e) => {
+  e.preventDefault();
+  this.props.searchUsers(this.state.text); //sends props to search function in App.js
+  this.setState({ text:'' }); //sets search back to blank
+};
+
+///even handles text box change, relevent only to this component
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.onSubmit} className='form'>
+          <input 
+            type='text' 
+            name='text' 
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange} //handles changes in Search bar allows typing
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Search;
