a369a9a58bfdc69e01268b1c35f97ed0f6fa2bf8
Charise Arter <mine@Charises-MacBook-Pro.local>
2020-06-03 11:18:51 -0500

    added avatar for followers

--- src/src/components/follows/FollowerItem.js
+++ src/src/components/follows/FollowerItem.js
@@ -3,11 +3,12 @@ import React from 'react'
 const FollowerItem = ({ follow }) => {
   console.log(follow)
   return (
-    <div className='card'>
-      
+    <div className='card all-center'>
+      <img src={follow.avatar_url} alt='avatar' className='round-img' style= {{ width: '50px' }} />
       <h3> 
         <a href={follow.html_url}> {follow.login} </a>  
       </h3>
+      
     </div>
   )
 }
