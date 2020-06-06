701ce668bdef118fccf5d5cd62fcfbbdedc9d7e6
unknown <clmonitor2013@gmail.com>
2020-06-06 16:47:53 -0400

    added state

--- user-card/src/components/App.js
+++ user-card/src/components/App.js
@@ -7,13 +7,33 @@ import Footer from "./Footer";
 class App extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      userName: "Paul",
+      bio: "Hello",
+      avitarUrl: "https://avatars2.githubusercontent.com/u/53325792?v=4",
+      followers: ""
+
+  };
+  }
+
+  setUser = (user) => {
+    //e.preventDefault();
+    const newUser = {
+      userName: user.userName,
+      bio: user.bio,
+      avitarUrl: user.avitarUrl,
+      followers: user.followers
+    };
+    this.setState(newUser);
+
   }
 
   render() {
     return (
       <div className="App">
-        <Header />
-        <Body />
+        <Header setUser={this.setUser}/>
+        <Body user={this.state}/>
         <Footer />
 
       </div>
