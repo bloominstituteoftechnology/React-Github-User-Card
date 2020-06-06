cebc246c3b8b34f6cf92b31ea61946577be4dfb6
unknown <clmonitor2013@gmail.com>
2020-06-06 16:59:42 -0400

    added follower card

---
+++ user-card/src/components/FollowerCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+class FollowerCard extends React.Component {
+    constructor() {
+        super();
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>Card</p>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+
+
+}
+
+export default FollowerCard;
