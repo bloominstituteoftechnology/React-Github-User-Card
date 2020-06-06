0b5b1797b8e5c1600da5fab44564b0ac6bf8b124
unknown <clmonitor2013@gmail.com>
2020-06-06 16:59:57 -0400

    added follower list

---
+++ user-card/src/components/FollowerList.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+class FollowerList extends React.Component {
+    constructor() {
+        super();
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>User list</p>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+
+    }
+
+    componentWillUnmount() {
+
+    }
+
+
+
+}
+
+export default FollowerList;
