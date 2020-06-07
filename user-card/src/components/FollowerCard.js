5ca669f8828780f8268216f37f7e1ee010f9cd77
unknown <clmonitor2013@gmail.com>
2020-06-06 20:57:19 -0400

    added axios

--- user-card/src/components/FollowerCard.js
+++ user-card/src/components/FollowerCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../css/index.css";
+import axios from "axios";
 
 
 class FollowerCard extends React.Component {
@@ -8,13 +9,39 @@ class FollowerCard extends React.Component {
         
     }
 
+    /*setUser = () => {
+        this.props.setUser(this.props.follower.login);
+    }*/
+
+    fetchUser = e => {
+        e.preventDefault();
+        axios
+            .get(`https://api.github.com/users/${this.props.follower.login}`)
+            .then(res => 
+                //console.log(res);
+                this.props.setUser({
+                    login: res.data.login,
+                    userName: res.data.name, 
+                    bio: res.data.bio,
+                    avitarUrl: res.data.avatar_url,
+                    followersList: []
+                })
+
+                   
+            )
+            .catch(err => console.log(err));
+
+            //reset the search box
+            //this.setState({ ...this.state, userName: "" }) 
+      };
+
 
     render() {
         return (
             
                 <div className="follower-card">
                     <img src={this.props.follower.avatar_url} alt="avitar"/>
-                    <p>{this.props.follower.login}</p>
+                    <p onClick={this.fetchUser}>{this.props.follower.login}</p>
                 </div>
             
         );
