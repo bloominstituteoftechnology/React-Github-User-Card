b8bf7d4bb98c962cd129a809c2d8193499f84c79
Katherine-Kumar <Katherine2bk@gmail.com>
2020-06-08 14:25:42 -0700

    created axios call, set it up to pass props

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from "./components/Github-Card";
+import axios from "axios";
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userCard: [],
+      followers: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://api.github.com/Kat2bk/")
+    .then(response => {
+      this.setState({
+        userCard: [{
+          img: response.data.avatar_url
+        }]
+      })
+    })
+    .catch(error => console.log("Sorry, something happened!", error));
+  }
+
+render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Github Users!</h1>
+      </div>
     </div>
   );
 }
+}
 
-export default App;
+export default App
