d1befa925caa0dcc17c242d897cea8bad86baa5d
JenVest2020 <jenvest2019@gmail.com>
2020-07-10 14:39:14 -0500

    API request resolved/working setState problem

--- usercards/src/App.js
+++ usercards/src/App.js
@@ -6,7 +6,15 @@ import User from './compnents/User';
 class App extends React.Component {
   constructor() {
     super();
-
+    this.state = {
+      githubUser: []
+    };
+  }
+  componentDidMount() {
+    fetch('https://api.github.com/users/JenVest2020')
+      .then(response => response.json())
+      .then(data => console.log('from CDM', data))
+    // .then(user => this.setState({ githubUser: user.res }))
   }
   render() {
     return (
