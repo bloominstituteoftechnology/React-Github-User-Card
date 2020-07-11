3aa346dc0b02d53f487601e4f576ab3ca626a869
PauloFurtunatoAlexandre <furtuna.alexandre@gmail.com>
2020-07-11 00:00:40 -0500

    finished MVP

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -8,10 +8,20 @@ class App extends React.Component {
 
     this.state = {
       myFollowers: [],
+      me: [],
     };
   }
 
   componentDidMount() {
+    fetch("https://api.github.com/users/PauloFurtunatoAlexandre")
+      .then((response) => response.json())
+      .then((paulo) => {
+        this.setState({
+          me: paulo,
+        });
+      })
+      .catch((err) => console.log(err));
+
     fetch("https://api.github.com/users/PauloFurtunatoAlexandre/followers")
       .then((res) => res.json())
       .then((followers) => {
@@ -24,38 +34,82 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <h1 className="text-center m-5">This is working!</h1>
-        <div className="row">
-          <div className="col-md-4 col-sm-12 m-5">
-            {this.state.myFollowers.map((follower) => (
-              <div className="follower-card card bg-light m-3 float-md-left" key={follower.login}>
-                <img
-                  className="card-img-top"
-                  src={follower.avatar_url}
-                  alt={follower.login}
-                />
-                <div className="card-body">
-                  <h3 className="card-title">{follower.login}</h3>
-                  <a
-                    className="btn btn-primary m-2"
-                    href={follower.organizations_url}
-                  >
-                    Organizations
-                  </a>
-                  <a
-                    className="btn btn-primary m-2"
-                    href={follower.followers_url}
-                  >
-                    Followers Url
-                  </a>
-                </div>
+      <>
+        <div className="container-fluid">
+          <div className="top-header">
+            <div className="row">
+              <div className="col">
+                <h1 className="text-center m-5">That's me!</h1>
               </div>
-            ))}
+            </div>
           </div>
-          <br />
+          <div className="row justify-content-center m-3">
+            <div className="card">
+              <img
+                className="card-img-top"
+                src={this.state.me.avatar_url}
+                alt=""
+              />
+              <div className="card-body">
+                <h3 className="card-title text-center">{this.state.me.login}</h3>
+                <a
+                  className="btn btn-primary m-2"
+                  href={this.state.me.organizations_url}
+                >
+                  Organizations
+                </a>
+                <a
+                  className="btn btn-primary m-2"
+                  href={this.state.me.followers_url}
+                >
+                  Followers Url
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+        <div className="container-fluid justify-content-center">
+          <div className="top-header">
+            <div className="row">
+              <div className="col">
+                <h1 className="text-center m-5">My Followers!</h1>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col m-5">
+              {this.state.myFollowers.map((follower) => (
+                <div
+                  className="follower-card card bg-light m-3 float-md-left"
+                  key={follower.login}
+                >
+                  <img
+                    className="card-img-top img-custom"
+                    src={follower.avatar_url}
+                    alt={follower.login}
+                  />
+                  <div className="card-body">
+                    <h3 className="card-title text-center">{follower.login}</h3>
+                    <a
+                      className="btn btn-primary m-2"
+                      href={follower.organizations_url}
+                    >
+                      Organizations
+                    </a>
+                    <a
+                      className="btn btn-primary m-2"
+                      href={follower.followers_url}
+                    >
+                      Followers Url
+                    </a>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <br />
+          </div>
+        </div>
+      </>
     );
   }
 }
