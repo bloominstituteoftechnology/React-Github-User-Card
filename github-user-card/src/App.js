8af9424cca0eb7637d98e5f713fe9cedb2e8c725
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-02 10:47:50 -0400

    Why did we have to redo this?

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardLink,
+  CardTitle,
+  CardSubtitle,
+} from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//this bitch gon' hold all the things, bruh.
+
+class App extends React.Component {
+  state = {
+    gitdata: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/avawing")
+      .then((res) => {
+        this.setState({ gitdata: res.data });
+      })
+      .catch((error) => {
+        console.log(`Everything is broken: ${error}`);
+      });
+  }
+
+
+  render() {
+    return (
+      <>
+        <h1>User Card</h1>
+        
+        <Card>
+        <CardBody>
+          <CardTitle>{this.state.gitdata.name}</CardTitle>
+          <CardSubtitle>{this.state.gitdata.login}</CardSubtitle>
+        </CardBody>
+        <img width="100%" src={this.state.gitdata.avatar_url} alt={this.state.gitdata.name} />
+        <CardBody>
+          <CardText>{this.state.gitdata.bio}</CardText>
+          <CardLink href={this.state.html_url}>GitHub</CardLink>
+
+        </CardBody>
+      </Card>
+        
+      </>
+    );
+  }
 }
 
 export default App;
