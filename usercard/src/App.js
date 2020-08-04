3ecb5a8d1dcf63912ac4529b0b975e4e29246281
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-04 11:47:18 -0400

    Prepare for separate API call to retrieve followers

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "fontsource-roboto";
 import requester from "easier-requests";
 
-import User from "./User";
+import {User, Follower} from "./utility";
 import UserDisplay from "./components/UserDisplay";
 
 export default class App extends React.Component {
@@ -20,10 +20,19 @@ export default class App extends React.Component {
 
   async retrieveUserData() {
     try {
-      const id = requester.createUniqueID();
+      // get primary user data
+      let id = requester.createUniqueID();
       await requester.get("https://api.github.com/users/HarryHenryGebel",
                           id);
-      this.setState({userData: new User(requester.response(id).data)});
+      const userData = requester.response(id).data;
+      this.setState({userData: new User(userData)});
+
+      // get followers
+      id = requester.createUniqueID();
+      await requester.get(userData.followers_url, id);
+      const followerData = requester.response(id).data;
+      console.log(followerData);
+      this.setState({userData: new User(userData, followerData)});
     }
     catch(error) {
       console.log(error);
--- usercard/src/utility.js
+++ usercard/src/utility.js
@@ -1,14 +1,16 @@
 export class User {
-  constructor(data = {}) {
+  constructor(data = {}, followers = {}) {
     if (data.avatar_url)
       this.avatar_url = data.avatar_url;
     else
       this.avatar_url = "./unknown.svg";
-    this.followers_url = data.followers_url;
     this.hireable = data.hireable;
     this.location = data.location;
     this.username = data.login;
     this.name = data.name;
+    // followers is filled from a different API request and may be
+    // empty if that request hasn't completed.
+    this.followers = followers;
   }
 }
 
