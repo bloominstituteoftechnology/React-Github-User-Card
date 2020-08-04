16b529175c0fcf6ea0cd9db7db09b2edff725eb4
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-04 03:24:38 -0400

    Retrieve user data on component mount

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -2,13 +2,32 @@ import React from "react";
 import "fontsource-roboto";
 import requester from "easier-requests";
 
+import User from "./User";
 
-export default App;
 export default class App extends React.Component {
   constructor () {
     super();
 
+    this.state = {
+      userData: new User()
     };
+  }
+
+  componentDidMount() {
+    this.retrieveUserData();
+  }
+
+  async retrieveUserData() {
+    try {
+      const id = requester.createUniqueID();
+      await requester.get("https://api.github.com/users/HarryHenryGebel",
+                          id);
+      this.setState({userData: new User(requester.response(id).data)});
+    }
+    catch(error) {
+      console.log(error);
+      throw error;
+    }
   }
 
   render () {
