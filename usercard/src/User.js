e8b1c27c9335d433c9c981bc509ea0b1f461bbfd
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-04 03:19:46 -0400

    Add User class

---
+++ usercard/src/User.js
@@ -0,0 +1,10 @@
+export default class User {
+  constructor(data = {}) {
+    this.avatar_url = data.avatar_url;
+    this.followers_url = data.followers_url;
+    this.hireable = data.hireable;
+    this.location = data.location;
+    this.login = data.login;
+    this.name = data.name;
+  }
+}
