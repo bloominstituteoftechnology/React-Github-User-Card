f29114759fa9fd3bec2c4e8812d0a0313b6d7795
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-04 17:19:17 -0400

    I hate so many things

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -18,16 +18,27 @@ class App extends React.Component {
     super();
     this.state = {
       gitdata: [],
+      gitusers: []
     };
   }
 
   componentDidMount() {
     axios
-      .all([axios.get('https://api.github.com/users/avawing'), axios.get('https://api.github.com/users/avawing/followers')])
-      .then(res => {
-
-          this.setState({gitdata: res.data})
-
+      .get("https://api.github.com/users/avawing")
+      .then((res) => {
+        this.setState([{gitdata: res.data}])
+        console.log(this.state.gitdata)
+        console.log(res.data)
+      })
+      .catch((error) => {
+        console.log(`Everything is broken: ${error}`);
+      });
+      axios
+      .get("https://api.github.com/users/avawing/followers")
+      .then((res) => {
+      this.setState({gitusers: res.data});
+      console.log(this.state.gitusers)
+      console.log(res.data)
       })
       .catch((error) => {
         console.log(`Everything is broken: ${error}`);
@@ -39,6 +50,21 @@ class App extends React.Component {
       <>
         <h1>User Card</h1>
         {this.state.gitdata.map((user) => {
+          return (
+            <Card key={user.id}>
+              <CardBody>
+                <CardTitle>{user.name}</CardTitle>
+                <CardSubtitle>{user.login}</CardSubtitle>
+              </CardBody>
+              <CardImg width="100%" src={user.avatar_url} alt={user.name} />
+              <CardBody>
+                <CardText>{user.bio}</CardText>
+                <CardLink href={user.html_url}>GitHub</CardLink>
+              </CardBody>
+            </Card>
+          );
+        })}
+        {this.state.gitusers.map((user) => {
           return (
             <Card key={user.id}>
               <CardBody>
