683aef480e01a88639670b74a6733ac58854dcd7
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-04 16:59:24 -0400

    This is a message!

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -14,40 +14,45 @@ import {
 //this bitch gon' hold all the things, bruh.
 
 class App extends React.Component {
-  state = {
-    gitdata: [],
-  };
+  constructor() {
+    super();
+    this.state = {
+      gitdata: [],
+    };
+  }
 
   componentDidMount() {
     axios
-      .get("https://api.github.com/users/avawing")
-      .then((res) => {
-        this.setState({ gitdata: res.data });
+      .all([axios.get('https://api.github.com/users/avawing'), axios.get('https://api.github.com/users/avawing/followers')])
+      .then(res => {
+
+          this.setState({gitdata: res.data})
+
       })
       .catch((error) => {
         console.log(`Everything is broken: ${error}`);
       });
   }
 
-
   render() {
     return (
       <>
         <h1>User Card</h1>
-        
-        <Card>
-        <CardBody>
-          <CardTitle>{this.state.gitdata.name}</CardTitle>
-          <CardSubtitle>{this.state.gitdata.login}</CardSubtitle>
-        </CardBody>
-        <img width="100%" src={this.state.gitdata.avatar_url} alt={this.state.gitdata.name} />
-        <CardBody>
-          <CardText>{this.state.gitdata.bio}</CardText>
-          <CardLink href={this.state.html_url}>GitHub</CardLink>
-
-        </CardBody>
-      </Card>
-        
+        {this.state.gitdata.map((user) => {
+          return (
+            <Card key={user.id}>
+              <CardBody>
+                <CardTitle>{user.name}</CardTitle>
+                <CardSubtitle>{user.login}</CardSubtitle>
+              </CardBody>
+              <CardImg width="100%" src={user.avatar_url} alt={user.name} />
+              <CardBody>
+                <CardText>{user.bio}</CardText>
+                <CardLink href={user.html_url}>GitHub</CardLink>
+              </CardBody>
+            </Card>
+          );
+        })}
       </>
     );
   }
