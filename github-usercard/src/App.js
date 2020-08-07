a6e4f384fec7ad2a46fac7520a73c049e641d741
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-08-06 19:06:04 -0700

    starting assignment

---
+++ github-usercard/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
