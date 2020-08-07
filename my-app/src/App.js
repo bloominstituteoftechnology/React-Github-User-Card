b0464b88c7946a9513f6fba90d6586ac94be9c1b
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-08-06 22:34:04 -0400

    attemping to get into API

---
+++ my-app/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import axios from "axios";
+import './App.css';
+
+
+class App extends React.Component { 
+  constructor(){
+    console.log("Constructor");
+    super();
+    this.state = {
+      person: []
+
+    };
+  }
+
+  componentDidMount(){
+    console.log("CDM running");
+    axios
+    .get(`https://api.github.com/users/`)
+    .then((res)=> {
+      this.setState({ person: res.data})
+      console.log("got the data!", this.state);
+    })
+    .catch((err)=> console.log("shit", err));
+
+  }
+  
+  
+ // componentDidUpdate(prevState, prevProps){
+ //  console.log("CDU", this.state.person)
+ // }
+
+
+
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+      <div> Hey </div>
+        
+       
+      </header>
+    </div>
+  );
+  };
+
+
+};
+
+export default App;
