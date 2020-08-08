a024e65d1d8d05330942711aa6f97820e26fd3cb
Elizabeth Hagag <lizjanegiles@gmail.com>
2020-08-07 20:11:46 -0500

    CDM Working, project rendering Data from API

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    console.log("Constructor")
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    console.log("CDM running")
+    axios
+    .get('https://api.github.com/users/lizhagag')
+    .then((res) => {
+      this.setState({users: res.data});
+      console.log(this.state)
+    })
+    .catch((err)=> console.log('Axios Err', err))
+  }
+
+  render () {
+    return (
+      <div>
+        <div>
+          <img width='150' height='150' src={this.state.users.avatar_url} alt='profile picture'/>
+          <div>
+            <p>Bio: {this.state.users.bio}</p>
+            <p>Followers: {this.state.users.followers}</p>
+            <p>Following: {this.state.users.following}</p>
+            <a href={this.state.users.html_url}>{this.state.users.name}Visit Their Profile!</a>
+          </div>
+        </div>
+      </div>
+      );
+  }
+ 
 }
 
 export default App;
