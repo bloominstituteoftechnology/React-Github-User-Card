0bcac827c319f9690d7785978fb0a2776badde23
noahbibb21 <noahbibb90@gmail.com>
2020-08-10 17:25:35 -0400

    finished MVP

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+import FollowCard from './component/FollowCard';
+import UserCard from './component/UserCard';
 
+const followersArray = [
+  'noahbibb21',
+  'BrandyBecker',
+  'nati-alvarez',
+  'kdneldor',
+  'alphaseinor'
+];
 
+class App extends React.Component {
+  state = {
+    userData: [],
+    followData: []
+  };
 
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  componentDidMount() {
+    axios
+    .get('"https://api.github.com/users/noahbibb21')
+    .then(res =>  {
+      this.setState({
+        userData: res.data
+      })
+    })
+followersArray.map(item => {
+  axios
+  .get(`https://api.github.com/users/${item}`)
+  .then(res => {
+    this.setState({
+      followData: [...this.state.followData, res.data]
+      })
+     })
+    })
+  }
+  
+  render() {
+    return (
+      <div className="App">
+      <h1>GitHub User Card</h1>
+      <UserCard userData = {this.state.userData}/>
+      <h2>Followers</h2>
+      {
+        this.state.followData.length === 0 && (<div>Loading</div>)
+      }
+      <FollowCard followers = {this.state.followData} />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
