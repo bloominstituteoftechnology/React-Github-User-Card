3fd51396b0ff4284c562c0ae0ccb622a5033517b
Galo Sandoval <galosan92@gmail.com>
2020-08-20 14:13:59 -0700

    added dependency

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios'
-import MyProfile from './MyProfile';
-import Cards from './Cards';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import MyProfile from "./MyProfile";
+import Cards from "./Cards";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       myCard: [],
-      cards: []
-    }
+      cards: [],
+    };
   }
 
   componentDidMount() {
-    axios.get('https://api.github.com/users/galosandoval')
-    .then(res => {
-      this.setState({
-        myCard: res.data
-      })
-    })
-      .then(res => {
-        return axios.get('https://api.github.com/users/galosandoval/followers')
-      })
-      .then(res => {
+    axios
+      .get("https://api.github.com/users/galosandoval")
+      .then((res) => {
         this.setState({
-          cards: res.data
-        })
+          myCard: res.data,
+        });
       })
-      .catch(err=> console.log(err, 'lol'))
+      .then((res) => {
+        return axios.get("https://api.github.com/users/galosandoval/followers");
+      })
+      .then((res) => {
+        this.setState({
+          cards: res.data,
+        });
+      })
+      .catch((err) => console.log(err, "lol"));
   }
 
   render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <MyProfile key={this.state.myCard.id} myCard={this.state.myCard}/>
-      </header>
-      <div>
-        {/* {this.state.cards.map(friends => (
+    return (
+      <div className="App">
+        <header className="App-header">
+          <MyProfile key={this.state.myCard.id} myCard={this.state.myCard} />
+        </header>
+        <div>
+          {/* {this.state.cards.map(friends => (
           <Cards key={friends.id} cards={this.state.cards} />
         ))} */}
-        <Cards key={this.state.cards.id} cards={this.state.cards} />
+          <Cards key={this.state.cards.id} cards={this.state.cards} />
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
