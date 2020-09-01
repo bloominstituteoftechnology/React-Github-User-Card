ff4fa50d846b0d9e088be21238182eace831e257
ajg7 <ajgebara@gmail.com>
2020-09-01 18:56:45 -0500

    made the app class component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Cards from "./components/Cards";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>GitHub Cards</h1>
-      <Cards />
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>GitHub Cards</h1>
+        <Cards />
+      </div>
+    );
+  }
 }
 
 export default App;
