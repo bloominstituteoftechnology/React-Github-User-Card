5f1be882d4ab35ef64e286e28da3492ca928e882
ajg7 <ajgebara@gmail.com>
2020-09-01 23:29:10 -0500

    rendered my name to the screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import Cards from "./components/Cards";
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users/ajg7")
+      .then(response => {
+        const userData = response.data
+        this.setState({
+          users: userData
+        })
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
 
   render() {
     return (
       <div className="App">
         <h1>GitHub Cards</h1>
-        <Cards />
+        <Cards users={this.state.users}/>
       </div>
     );
   }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,12 +3,13 @@ import React from "react"
 
 const Cards = props => {
 
-
-        return(
-            <div className="cards">
-            
+    return(
+        <>
+            <div key={props.users.id}>
+                <h3>{props.users.name}</h3>
             </div>
-        )
+        </>
+    );
 }
 
 
