aa3dc369a1a231ce95bf87b9d1e0c248870c07ab
ajg7 <ajgebara@gmail.com>
2020-09-01 23:41:33 -0500

    added style to the cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,11 @@
 import React from "react"
+import "../cards.css"
 
 
 const Cards = props => {
 
     return(
-        <>
+        <section className="card-section">
             <div key={props.users.id} className="cards">
                 <h3>{props.users.name}</h3>
                 <img src={props.users.avatar_url} alt={`${props.users.name}'s avatar`} />
@@ -12,7 +13,7 @@ const Cards = props => {
                 <p>{props.users.location}</p>
                 <p>{props.users.followers} Followers & {props.users.following} Following</p>
             </div>
-        </>
+        </section>
     );
 }
 
