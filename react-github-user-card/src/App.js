3b39abb06225a8090aa4be8927ce5c5b60226550
Shenandoah Veele <64814835+Jamakura@users.noreply.github.com>
2020-09-07 12:02:18 -0400

    app set up. Onto functionality

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 
 import UserCard from "./components/UserCard";
 import "./App.css";
+import { render } from "@testing-library/react";
 
-function App() {
-  return <h1> Hello from App! </h1>;
+class App extends Component {
+  constructor() {
+    super();
+    //this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <UserCard />
+      </div>
+    );
+  }
 }
 
 export default App;
