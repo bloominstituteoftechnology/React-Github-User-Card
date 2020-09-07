64bd95510f594542804edaff81b9393eed1083fb
Shenandoah Veele <64814835+Jamakura@users.noreply.github.com>
2020-09-07 11:36:57 -0400

    getting started

---
+++ react-github-user-card/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import UserCard from "./components/UserCard";
+import "./App.css";
+
+function App() {
+  return <h1> Hello from App! </h1>;
+}
+
+export default App;
---
+++ react-github-user-card/src/components/UserCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function UserCard() {
+  return <h1>Hello from UserCard</h1>;
+}
+
+export default UserCard;
