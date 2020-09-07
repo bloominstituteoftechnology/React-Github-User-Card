e7a482f0a2ee1ed0e1e0af1cde8070d814cfd695
Shenandoah Veele <64814835+Jamakura@users.noreply.github.com>
2020-09-07 16:46:48 -0400

    some styling done

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,9 +1,28 @@
-import React, { Component } from "react";
+//importing dependancies
 
+import React, { Component } from "react";
+import styled from "styled-components";
+import "./App.css";
 //import axios from "axios";
 
+// importing files
 import UserCard from "./components/UserCard";
-import "./App.css";
+
+//styling
+
+const Body = styled.div`
+  background-image: url("https://images.unsplash.com/photo-1431576901776-e539bd916ba2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80");
+`;
+
+const Headline = styled.h1`
+  font-size: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: black;
+  color: darkorange;
+  padding-bottom: 40px;
+`;
 
 class App extends Component {
   //initailize state
@@ -18,6 +37,8 @@ class App extends Component {
     // setting state here
     console.log(" SV: app.js: App, CDM: CDM running");
 
+    //   fetching user profile
+
     fetch("https://api.github.com/users/jamakura")
       .then((res) => res.json())
       .then((json) => {
@@ -25,6 +46,8 @@ class App extends Component {
         console.log("SV: App.js: CMD: success!", this.state.userInfo);
       })
       .catch((err) => console.log(err));
+
+    //   fetching user followers
 
     fetch("https://api.github.com/users/jamakura/followers")
       .then((res) => res.json())
@@ -35,16 +58,18 @@ class App extends Component {
       .catch((err) => console.log(err));
   }
 
+  //render to DOM
+
   render() {
     console.log("sv: App.js: App, render");
     return (
-      <div>
-        <h1>Github User Profiles!</h1>
+      <Body>
+        <Headline>Github User Profiles!</Headline>
         <UserCard userInfo={this.state.userInfo} />
         {this.state.followers.map((follower) => {
           return <UserCard key={follower.id} userInfo={follower} />;
         })}
-      </div>
+      </Body>
     );
   }
 }
--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -1,17 +1,32 @@
+//importing dependancies
+
 import React from "react";
+import styled from "styled-components";
+
+//styling cards
+
+const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  background: chocolate;
+  margin: 25px 400px 25px 200px;
+  width: 50%;
+  height: auto;
+`;
 
 const UserCard = (props) => {
   console.log(props);
 
   return (
-    <div>
+    <Card>
       <img src={props.userInfo.avatar_url} />
       <div>
-        <h3>{props.userInfo.login}</h3>
+        <h3>{props.userInfo.name}</h3>
+        <p>{props.userInfo.login}</p>
         <p>{props.userInfo.location}</p>
         <a href={props.userInfo.html_url}>{props.userInfo.login}'s profile</a>
       </div>
-    </div>
+    </Card>
   );
 };
 
