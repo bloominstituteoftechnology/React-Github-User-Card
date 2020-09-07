4cdaf33aa4f273197ad4bf973e0e37da65dfa396
Shenandoah Veele <64814835+Jamakura@users.noreply.github.com>
2020-09-07 15:58:15 -0400

    mvp hit, just need to style

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
 
+//import axios from "axios";
+
 import UserCard from "./components/UserCard";
 import "./App.css";
-import { render } from "@testing-library/react";
 
 class App extends Component {
-  constructor() {
-    super();
-    //this.state = {};
+  //initailize state
+  state = {
+    userInfo: {},
+    followers: [],
+  };
+
+  componentDidMount() {
+    // after initial render
+    //api call here https://api.github.com/users/jamakura
+    // setting state here
+    console.log(" SV: app.js: App, CDM: CDM running");
+
+    fetch("https://api.github.com/users/jamakura")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({ userInfo: json });
+        console.log("SV: App.js: CMD: success!", this.state.userInfo);
+      })
+      .catch((err) => console.log(err));
+
+    fetch("https://api.github.com/users/jamakura/followers")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({ followers: json });
+        console.log("SV: App.js: CMD: success!", this.state.followers);
+      })
+      .catch((err) => console.log(err));
   }
 
   render() {
+    console.log("sv: App.js: App, render");
     return (
       <div>
-        <UserCard />
+        <h1>Github User Profiles!</h1>
+        <UserCard userInfo={this.state.userInfo} />
+        {this.state.followers.map((follower) => {
+          return <UserCard key={follower.id} userInfo={follower} />;
+        })}
       </div>
     );
   }
--- react-github-user-card/src/components/UserCard.js
+++ react-github-user-card/src/components/UserCard.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-function UserCard() {
-  return <h1>Hello from UserCard</h1>;
-}
+const UserCard = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <img src={props.userInfo.avatar_url} />
+      <div>
+        <h3>{props.userInfo.login}</h3>
+        <p>{props.userInfo.location}</p>
+        <a href={props.userInfo.html_url}>{props.userInfo.login}'s profile</a>
+      </div>
+    </div>
+  );
+};
 
 export default UserCard;
