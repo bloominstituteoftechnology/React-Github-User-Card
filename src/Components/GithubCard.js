c2a47dd7af5f7fefca96aaeac88103aa60b518ab
unknown <abdinajibmohamud@gmail.com>
2020-09-17 13:30:12 -0700

    cleaner version to deploy

--- src/Components/GithubCard.js
+++ src/Components/GithubCard.js
@@ -1,50 +1,42 @@
-import React from  'react'
-import GithubFollowers from "./GithubFollowers"
-import GithubCalender from 'github-calendar'
+import React from "react";
+import GithubFollowers from "./GithubFollowers";
 
+const GithubCard = (props) => {
+  const url = `https://grass-graph.moshimo.works/images/${props.githubData.login}.png`;
 
- const GithubCard = (props) => {
-    const url = `https://grass-graph.moshimo.works/images/${props.githubData.login}.png`
-    console.log(url)
-
-    return (
-        <div className="githubcard">
-           <img src={props.githubData.avatar_url} alt="pfp"/> 
-           <div className="githubcardinfo">
-            
-            <h1>{props.githubData.name}</h1>
-            <p><i>{props.githubData.username}</i></p>
-            <p>Location : {props.githubData.location}</p>
-            <p>Profile : <a href={props.githubData.html_url}>{props.githubData.html_url}</a></p>
-            <p>Followers: {props.githubData.followers}  Following: {props.githubData.following}</p>
-            <p>Bio : "{props.githubData.bio}"</p>
-            <p>Contributions:</p>  <img src={url} alt="lol" />
-            
-                
-
-                <div className="followerscont">
-                <h2>Followers :</h2> 
-                <div className="followerscard">
-                {props.githubFollowers.map((item)=>(
-                    <GithubFollowers
-                        key={item.id}
-                        item={item}
-                        />
-                        ))} 
-                </div>
-                
-
-                </div>
-            </div>
-          
-           
-
-
-           
+  return (
+    <div className="githubcont">
+      <h2> Results:</h2>
+      <div className="githubuser">
+        <div className="image">
+          <img src={props.githubData.avatar_url} alt="pfp" />
         </div>
-    )
-}
+        <div className="githubcardinfo">
+          <h1>{props.githubData.name}</h1>
+          <h3>Location : {props.githubData.location}</h3>
+          <h3>
+            Profile :{" "}
+            <a href={props.githubData.html_url}>{props.githubData.html_url}</a>
+          </h3>
+          <h3>
+            Followers: {props.githubData.followers} Following:{" "}
+            {props.githubData.following}
+          </h3>
+          <h3>Bio : "{props.githubData.bio}"</h3>
+          <h3>Contributions:</h3> <img className="cont" src={url} alt="lol" />
+        </div>
+      </div>
 
-export default GithubCard 
+      <div className="followerscont">
+        <h2>Followers :</h2>
+        <div className="followerscard">
+          {props.githubFollowers.map((item) => (
+            <GithubFollowers key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
+export default GithubCard;
--- src/Components/GithubFollowers.js
+++ src/Components/GithubFollowers.js
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const GithubFollowers = (props) => {
-    const followurl = `https://grass-graph.moshimo.works/images/${props.item.login}.png`
-    return(
-        <div className="githubcard">
-        <img src={props.item.avatar_url} alt="pfps"/> 
-        <div className="githubcardinfo">
-         
-         <h1><i>{props.item.login}</i></h1>
-         <p><i>{props.item.username}</i></p>
-         
-         <p>Profile : <a href={props.item.html_url}>{props.item.html_url}</a></p>
-         <p>Contributions :</p> <img src={followurl} alt="cont" />
+  const followurl = `https://grass-graph.moshimo.works/images/${props.item.login}.png`;
+  return (
+    <div className="githubcardfollower">
+      <div className="image">
+        <img src={props.item.avatar_url} alt="pfps" />
+      </div>
 
-         </div>
-
-
-
-        
-     </div>
-    )
-}
-export default GithubFollowers
+      <div className="githubcardinfo">
+        <h1>{props.item.login}</h1>
+        <h3>
+          Profile : <a href={props.item.html_url}>{props.item.html_url}</a>
+        </h3>
+        <h3>Contributions Graph :</h3>{" "}
+        <img className="cont" src={followurl} alt="cont" />
+      </div>
+    </div>
+  );
+};
+export default GithubFollowers;
