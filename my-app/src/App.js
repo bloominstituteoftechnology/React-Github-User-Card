f7ca3162d47772d4de3c09e73f8ff26edcebf954
Sam Lalli <2sam.lalli@gmail.com>
2020-09-29 16:25:53 -0600

    app rendered, awaitng styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Followers from './Followers'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state ={
+      user: []
+    }
+  }
+
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/sam-lalli')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+        user: data
+        })
+    })
+    .catch(err => console.log(err))
+}
+
+  render() {
+    return(
+      <div>
+        <h1>GitHub Usercard</h1>
+        <div className="my-card">
+          <h2>{this.state.user.name}</h2>
+          <img  width='200'src={this.state.user.avatar_url} alt={this.state.user.name}/>
+          <p>Bio: {this.state.user.bio}</p>
+          <p>Location: {this.state.user.location}</p>
+          <p>Username: {this.state.user.login}</p>
+        </div>
+        <Followers user={this.state.user} />
+      </div>
+    )
+  }
+
+
+
 }
 
 export default App;
---
+++ my-app/src/UserCard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class UserCard extends React.Component {
+
+
+
+
+    render(){
+        return(
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+
+export default UserCard
