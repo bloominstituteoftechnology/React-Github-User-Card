356c1998b26ccfba28913d8457e7002bf42191af
Francis Bonacic <francisbonacic@icloud.com>
2020-09-29 21:45:06 -0700

    finished mvp

--- user-app-project/src/App.js
+++ user-app-project/src/App.js
@@ -2,42 +2,107 @@ import React from 'react';
 import './App.css';
 import axios from 'axios'
 
+
 class App extends React.Component {
+  state= {
+    user: {},
+    followers: [],
+    userText:'',
+    error:'',
 
-//initial state / API fetch will go here // 
+  };
+  componentDidMount(){
+    axios.get('https://api.github.com/users/fibonacci85')
+    .then(res => {
+      console.log(res.data)
+      this.setState({
+        user: res.data
+      });
+    })
+    .catch(err => console.log(err));
+    axios.get('https://api.github.com/users/fibonacci85/followers')
+    .then(res=>{
+      this.setState({
+        followers:res.data
+      });
+    })
+    .catch(err=> console.log(err));
+  }
 
-state = {
-  followers: []
-}
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.user !== this.state.user){
+        this.fetchUsers= e => {
+          e.preventDefault()
+          axios.get(`https://api.github.com/users/${this.state.userText}`)
+        .then(res =>{
+          this.setState({
+            user:res.data
+          });
+        })
+        .catch(err => console.log(err));
+        axios.get(`https://api.github.com/users/${this.state.userText}/followers`)
+        .then(res =>{
+          this.setState({
+            followers:res.data
+          });
+        })
+        .catch(err => console.log(err));
+    }
+        }
+  }
 
-componentDidMount() {
-  axios
-    .get('https://api.github.com/users/fibonacci85/followers')
-  .then((res) => {
- console.log("response", res.data);
- this.setState({
-   followers:[...this.state.followers, res.data],
- });
-})
-.catch((err) => {
-  console.log("something went wrong", err);
-});
+
+handleChanges = e =>{
+  this.setState({
+    userText: e.target.value
+  });
 };
 
-
-  render(){
+render() {
   return (
-    <div className="App">
+    <div className='App'>
+
       <h1>GitHub Users</h1>
 
-      <div className="userCard">
-        {this.state.followers.map((user) => (
-          <p>{this.state.user}</p>
-        ))}
+      <div className="user">
+          <h1>{this.state.user.login}</h1>
+          <div className="userBox">
+              <img src={this.state.user.avatar_url} width="200"/>
+              <div className="about">
+                  <p>Name:{this.state.user.name}</p>
+                  <p>About:{this.state.user.bio}</p>
+                  <p>Followers:{this.state.user.followers}</p>
+                  <p>Following:{this.state.user.following}</p>
+                  <p>Location:{this.state.user.location}</p>
+              </div>
+          </div>
       </div>
+
+      <br/>
+
+      <form>
+      <input
+        type="text"
+        placeholder="Enter Username"
+        value={this.state.userText}
+        onChange={this.handleChanges}
+      />
+      <button onClick={this.fetchUsers}>Get Users</button>
+      </form>
+
+      <br/>
+
+      {/* {this.state.error && <p style={{color: 'red' }}>{this.state.error}</p>} */}
+        
+        {this.state.followers.map(follower=>(
+          <div className='followers'>
+            <img src= {follower.avatar_url} width="200" />
+            <p>{follower.login}</p>
+          </div>
+        ))}
+
     </div>
   );
 }
 }
-
 export default App;
