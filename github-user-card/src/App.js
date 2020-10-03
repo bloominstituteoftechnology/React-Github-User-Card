03392eda8c357390fd3cb7b133e6e4af9864bc9f
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-10-03 16:08:59 -0400

    added followers to app.

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -24,10 +24,12 @@ class App extends React.Component {
   
 
     axios
-      .get( "https://api.github.com/users/tbbcoach/followers" )
-  .then((res) => {
+      .get("https://api.github.com/users/tbbcoach/followers")
+      .then((res) => {
         console.log(res);
+        this.setState({ followers: res.data })
       })
+      .catch((err) => console.log('error', err));
   }
 
   componentDidUpdate(prevState, prevProps) {
@@ -40,13 +42,21 @@ class App extends React.Component {
     console.log('render invoked')
     return (
       <div className="app">
-        <h1>GIT HUB USERS</h1>
+        <h1>GIT HUB USER</h1>
         <div>
           <h2>{this.state.user.name}</h2>
           <img className="pic" src={this.state.user.avatar_url} alt="user" />
           <h3>User name: {this.state.user.login}</h3>
           <h4>Location: {this.state.user.location}</h4>
           <p>Bio: {this.state.user.bio}</p>
+        </div>
+        <h1>GIT HUB FOLLOWERS</h1>
+        <div>
+          {this.state.followers.map((follower) => (
+            <p>Name - {follower.login}
+            <img src={follower.avatar_url} alt='follower' />
+            </p>  
+    ))}
         </div>
       </div>
     );
