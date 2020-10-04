2b3a36cc4d194c4f5a9b475a0cce64fd66601ea7
Damaris Garcia <damaris-garcia@lambastudents.com>
2020-10-04 19:51:21 -0400

    Styling Complete MVP Met

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -29,35 +29,36 @@ class App extends React.Component {
           this.setState({ followers: res.data})
         })
   }
-
-
-
   render() {
     console.log("Render Invoked");
     return (
       <div className="App">
-        <h1>Github User {this.state.users.name}</h1>
+      <div className="userInfo">
+        <img src={this.state.users.avatar_url}  alt="" />
+      <div className="userProfile">
+        <h1>Github User: {this.state.users.name}</h1>
         <h2>User Name: {this.state.users.login}</h2>
-        <img src={this.state.users.avatar_url} alt="" />
-        <p>{this.state.users.bio}</p>
-      <div className="link">
-        <p>Follow Me:</p>
-        <a href="githublink">{this.state.users.html_url}</a>
+     <div className="userLink">
+        <h3>Followers: {this.state.users.followers}</h3>
+        <h3>Following: {this.state.users.following}</h3>
       </div>
+        <p>{this.state.users.bio}</p>
+        <a href="https://github.com/dgarcialambda">Follow Me: {this.state.users.html_url}</a>
+      </div>
+      </div>
+      
       <div className="Followers">
        {this.state.followers.map((follower) => (
-         <div className="followers-info">
-         <img src={follower.avatar_url} className="followersImg" alt="" />
-          <h2>{follower.login}</h2>
-          <a href="followersLink"> {follower.html_url}</a>
-
-         </div>
-      ))}
-      
-        
-
-      </div>
-      </div>
+         <div className="followersInfo">
+            <img src={follower.avatar_url} className="followersImg" alt="" />
+            <div className="followersProfile">
+            <h2>Github User: {follower.login}</h2>
+            <a href="html_url">{follower.html_url} </a> 
+          </div>
+          </div>
+          ))}
+          </div>
+          </div>
     )
   }
 }
