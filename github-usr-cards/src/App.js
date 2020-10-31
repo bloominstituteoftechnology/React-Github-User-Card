86a435665f5ea8fbfd786e9dfdac1c16a6a4a19d
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-10-31 04:33:47 -0700

    Searches the users avatar img

---
+++ github-usr-cards/src/App.js
@@ -0,0 +1,82 @@
+import React from "react";
+import "./styles.css";
+
+/*
+  The github react cards assignment
+*/
+class App extends React.Component {
+  state = {
+    userimg: "",  
+    newusr: ""
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/extrude575757")
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.status === "success") {
+          this.setState({ userimg: json.avatar_url });
+        } else {
+          console.error("App.js CompDidMount: jsonstatus false  error fetching githubbers though its forced into a set state anyway: ", json);
+          this.setState({ userimg: json.avatar_url });
+        }
+      })
+      .catch((err) => console.error("App.js CompDidMount: Major error from catch function ", err));
+  // Fetch followers here as well https://api.github.com/users/< Your github name >/followers
+  
+    }
+  /*Some names
+   bigknell','tetondan',
+'dustinmyers', 'justsml', 'luishrd', 'snowcoding','james-coulter
+        */
+  handleUserChange = (e) => {
+    this.setState({
+      newusr: e.target.value
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.newusr !== this.state.newusr) {
+     
+console.log("cdu: here prevProps"+prevProps+" prevState"+prevState);
+    }
+
+  }
+
+  handleSetNewUser = (e) => { 
+    fetch(`https://api.github.com/users/${this.state.newusr}`)
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.status === "success") {
+          this.setState({ userimg: json.avatar_url });
+        } else {
+          console.error("error fetching githubbers: handleSetNewUser forcing it any way", json);
+          this.setState({ userimg: json.avatar_url });
+        }
+      })
+      .catch((err) => console.error("App.js HandleSetNewUser You've got errors: ", err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Checkout Someone's Github</h1>
+        <div>
+          Username:{" "}
+          <input
+            type="text"
+            value={this.state.newusr}
+            onChange={this.handleUserChange}
+          />
+          <button onClick={this.handleSetNewUser}>newusers information</button>
+        </div>
+        <div>Users</div>
+       
+           <img  alt="Github User Avatar Image" src={this.state.userimg} width="300px" />
+  
+      </div>
+    );
+  }
+}
+
+export default App;
