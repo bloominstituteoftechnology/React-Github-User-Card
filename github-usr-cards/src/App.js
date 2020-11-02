8d158bc5de15fa86dd2459ad02bd88f7c46a433c
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-11-02 14:51:48 -0800

    MVP with components and followers made

--- github-usr-cards/src/App.js
+++ github-usr-cards/src/App.js
@@ -27,45 +27,6 @@ class App extends React.Component {
 
   componentDidMount() {
     console.log("appjs inside componentdidmount "+this.state.submitting);
-    
-    // The default fetch for the example beginning search
-  //   fetchAppD()
-  //     .then((json) => {
-  //       if (json.status === "success") {
-          
-  //         this.setState({ usersname: json.name,username: json.login
-  //           ,userimg: json.avatar_url });
-          
-  //       } else {
-  //         console.error("App.js CompDidMount: jsonstatus false  error fetching githubbers though its forced into a set state anyway: ", json);
-  //         this.setState({ usersname: json.name,username: json.login
-  //           ,userimg: json.avatar_url });
-  //       }
-  //     })
-  //     .catch((err) => console.error("App.js CompDidMount: Major error from catch function ", err));
-  // // Fetch followers here as well https://api.github.com/users/< Your github name >/followers
-  
-      // if(this.state.newusr !== null){
-      //   console.log('appjs componentdidmount statenewusr was not null')
-      //   fetchNewu(this.state.newusr )
-      //   .then((json) => {
-      //     if (json.status === "success") {
-      //       this.setState({ usersname: json.name,username: json.login
-      //       ,userimg: json.avatar_url });
-             
-      //     } else {
-      //       console.error("App.js No success error fetching githubbers: handleSetNewUser forcing it any way", json);
-      //       this.setState({ usersname: json.name,username: json.login
-      //         ,userimg: json.avatar_url });
-      //     }
-      //   })
-      //   .catch((err) => console.error("App.js HandleSetNewUser You've got errors: ", err));
-      
-        
-      
-      // }
-      
-      
 
     }
   /*Some names
@@ -83,14 +44,10 @@ class App extends React.Component {
 
     console.log("appjs inside componentdidupdate"+prevState.username);
     if ( this.state.newusr !== this.state.username && this.state.username !== null) {
-    // Setting username to null here will trigger the rerender we need to show the currently searched name
-    // this.setState({newusr:this.state.username});  
+
     this.setState({username:null})
-    //  if(this.state.submitting === null){
-    //    this.setState({submitting:true})
-    //  }
-    //  prevState.setState({newusr:this.state.newusr});
-    // prevState.newusr = this.state.newusr;
+
+
 console.log(`Appjs cdu: this.state ${this.state.newusr} prevProps ${prevProps.newusr} prevState ${prevState.newusr}` );
     }
 
@@ -99,38 +56,7 @@ console.log(`Appjs cdu: this.state ${this.state.newusr} prevProps ${prevProps.ne
   handleSetNewUser = (newu) => { 
 console.log('Appjs through the handleSetNewUser to set newusr then fetch newu')
     
-    /*
-fetchNewu(newu)
-      .then((json) => {
-        // This is so the search does not have to be case sensitive for the user
-        if(newu != json.login){
-          this.setState({newusr:json.login})
-        }else{
-          this.setState({
-            newusr: newu
-          })
-        }
-        // if (json.status === "success") {
-        //   this.setState({ usersname: json.name,username: json.login
-        //   ,userimg: json.avatar_url });
-           
-        // } else {
-        //   console.error("App.js No success error fetching githubbers: handleSetNewUser forcing it any way", json);
-        //   this.setState({ usersname: json.name,username: json.login
-        //     ,userimg: json.avatar_url });
-        // }
-        this.setState({username: json.login, 
-          usersname: json.name, userimg:json.avatar_url,
-        bio: json.bio, created_at: json.created_at, location: json.location })
 
-        console.log(json);
-         
-      
-      })
-      .catch((err) => console.error("App.js HandleSetNewUser You've got errors: ", err));
-     
-
-    */
 
       Promise.all([
         fetch(`https://api.github.com/users/${newu}`)
