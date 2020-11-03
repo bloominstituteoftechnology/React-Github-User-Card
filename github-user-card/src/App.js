afafaa2914fe1653842a4c985c742f7b814760bf
Shenandoah Veele <64814835+Jamakura@users.noreply.github.com>
2020-11-02 22:34:18 -0500

    initial app set up

---
+++ github-user-card/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import UserCard from './components/UserCard'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserCard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ github-user-card/src/components/UserCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const UserCard = () => {
+
+    return (
+       <h1>hi from usercard</h1>
+    )
+}
+
+export default UserCard
