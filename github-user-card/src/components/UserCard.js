2b3b80b471d67a6ab25d14810f1a5d5ae6d97916
Shenandoah Veele <64814835+Jamakura@users.noreply.github.com>
2020-11-04 21:44:50 -0500

    mvp

--- github-user-card/src/components/UserCard.js
+++ github-user-card/src/components/UserCard.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
-const UserCard = () => {
+//styling cards
 
+const Card = styled.div`
+  display: flex;
+  justify-content: center;
+  background: chocolate;
+  margin: 25px 400px 25px 200px;
+  width: 50%;
+  height: auto;
+`;
+const UserCard = (props) => {
+    console.log(props);
+  
     return (
-       <h1>hi from usercard</h1>
-    )
-}
+      <Card>
+        <img src={props.userInfo.avatar_url} />
+        <div>
+          <h3>{props.userInfo.name}</h3>
+          <p>{props.userInfo.login}</p>
+          <p>{props.userInfo.location}</p>
+          <a href={props.userInfo.html_url}>{props.userInfo.login}'s profile</a>
+        </div>
+      </Card>
+    );
+  };
+  
+  export default UserCard;
 
-export default UserCard
