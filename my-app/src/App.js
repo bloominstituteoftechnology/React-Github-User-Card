f2879661a5f4522aa0191282a4cd110a49f51d6b
cmirza <mail@cmirza.com>
2020-12-01 16:21:18 -0800

    followers rendering to screen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -33,13 +33,27 @@ class App extends React.Component {
 
 
   render(){
-    console.log(this.state)
     return (
       
       <div className='App'>
         <div>
           <h1>GitHub Followers</h1>
         </div>
+        <div>
+          <img src={this.state.user.avatar_url} alt={this.state.user.login}/>
+          <p>{this.state.user.name}</p>
+          <p>{this.state.user.login}</p>
+          <span>Profile: <a href={this.state.user.html_url}>{this.state.user.html_url}</a></span>
+          <p>Followers: {this.state.user.followers}</p>
+          <p>Following: {this.state.user.following}</p>
+        </div>
+        
+        {this.state.followers.map(follower =>
+          <div className='card' key={follower.id}>
+            <p>{follower.login}</p>
+          </div> 
+          )}
+
       </div>
     );
   }
