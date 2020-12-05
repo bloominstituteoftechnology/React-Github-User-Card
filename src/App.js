e940396d78cbfa2ccd95c846a0f90902facc4d88
Greyson <greyson.r.hamilton@gmail.com>
2020-12-04 19:42:20 -0500

    I might be getting this

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component() {
+  constructor() {
+    super();
+
+    this.state = {
+
+      user: {},
+      username: ''
+
+    }
+
+  }
+
+  fetchUser = () => {
+
+    alert(`Fetching ${this.state.username} user.`);
+
+    axios.get(`https://api.github.com/users/${this.state.username}`)
+
+    .then((res) => {
+        this.setState({
+            ...this.state,
+            user: res.data.value
+        });
+    })
+
+    .catch((err) => {
+        console.log(err);
+    })
+
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state, 
+      username: e.target.login
+    })
+  }
+
+  componentDidMount() {
+
+    axios.get('https://dog.ceo/api/breed/hound/images')
+
+    .then((res) => {
+        console.log(res);
+        this.setState({
+            ...this.state,
+            user: res.data.value
+        });
+
+    })
+
+    .catch((err) => {
+        console.log(err);
+    })
+
+}
+
+  render() {
+
+    return (
+
+      <div className="App">
+        <div>
+          <h1>Greetings GitHub User</h1>
+          <h2>Please enter the name of the user you wish to view.</h2>
+        </div>
+        <div>
+          <input 
+            type='text' 
+            placeholder = 'Enter GitHub account name here.' 
+            value = {this.state.username}
+            onChange = {this.handleChange}
+           />
+          <button onClick = {this.fetchUser}>
+            Submit
+          </button>
+        </div>
+      </div>
+
+    );
+
+  }
+
 }
 
 export default App;
