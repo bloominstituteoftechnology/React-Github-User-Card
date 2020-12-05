3135b26019526de4470f0c4ca8f7ce7008002f15
Jean Carlos  Ramos <rmscrlos@gmail.com>
2020-12-05 13:56:15 -0500

    finished whole app

--- github-user-card/src/components/Card.js
+++ github-user-card/src/components/Card.js
@@ -2,7 +2,27 @@ import React, { Component } from 'react';
 
 export class Card extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<div className="card">
+				<div className="user__img">
+					<img width="150" src={this.props.userData.avatar_url} alt="user's avatar" />
+				</div>
+				<div className="user__info">
+					<div className="user__info__top">
+						<h3>{this.props.userData.name}</h3>
+						<p>username: {this.props.userData.login}</p>
+					</div>
+					<div className="user__info__bottom">
+						<p>{this.props.userData.bio}</p>
+						<p className="followers">
+							<strong>Followers: </strong>
+							{this.props.userData.followers}
+						</p>
+						<button onClick={this.props.fetchFollowers}>Followers</button>
+					</div>
+				</div>
+			</div>
+		);
 	}
 }
 
