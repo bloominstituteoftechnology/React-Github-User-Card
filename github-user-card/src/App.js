164022175f844c24ecd9ef9dcd656309aa4e2c19
Lakendria <kendra939@gmail.com>
2020-12-06 12:50:43 -0500

    set up axios call

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from "axios";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const user = [];
+const followers= [];
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username:"",
+      user:[],
+      followers:[]
+    };
+  };
+  handleChange = (e) =>{
+console.log(e.target.value)
+this.setState({
+  ...this.state,
+  username:e.target.value
+});
+  };
+
+  componentDidMount(){
+    console.log("Component did mount");
+    axios
+    .get(`https://api/github.com/users/kendra4227`)
+    .then((res)=>{
+      this.setState({
+        ...this.state,
+        user:res.data.message
+      })
+    .catch((err)=> console.log("Error has occured",err));
+    });
+  };
+
+  componentDidUpdate(PrevState){
+    if(PrevState !== this.state.user){
+      console.log("User has changed")
+    }
+  };
+
+  getUser = () =>{
+    axios
+    .get(`https://api.githiub.com/users/$(this.state.username)`)
+
+    .then((res)=>{
+      this.setState({
+        ...this.state,
+        username:"",
+        user:res.data.message
+      });
+    })
+    .catch((err) => console.log("Error occured again",err))
+  };
+
+  render() {
+    return(
+      <div className ="App">
+        <h1>Github User</h1>
+      </div>
+    );
+  } 
 }
 
 export default App;
