598f28a1e8e2fb441810404327b67ffba9f597e1
Allison Castaneda <allraec@yahoo.com>
2020-12-12 19:06:50 -0600

    mvp

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Card from './components/Card'
+import Card2 from './components/Card2'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: {},
+      followers: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/allraec')
+    .then(res => {
+      this.setState({
+        ...this.state,
+        user: res.data
+      })
+    })
+    .catch(err => console.log(err));
+
+    axios.get('https://api.github.com/users/allison/followers')
+    .then(res => {
+      this.setState({
+        ...this.state,
+        followers: res.data
+      })
+    })
+    .catch(err => console.log(err));
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if(this.state.user !== {}) {
+      console.log(this.state.user)
+    }
+    if(prevState.followers.length !== this.state.followers.length) {
+      console.log(this.state.followers)
+    }
+  }
+
+/*   componentDidUpdate(prevProps, prevState) {
+    if(prevState.user.length !== this.state.user.length) {
+      console.log(this.state.user)
+    }
+  } */
+  render() {
+    return(
+      <div className='App'>
+        <h1>Github User Card</h1>
+        <Card user={this.state.user}/>
+        <h2>Followers:</h2>
+        {
+          this.state.followers.map((user) => {
+            return <Card2 user={user}/>
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-github-user-card/src/components/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Card = (props) => {
+
+    return(
+        <div className="card">
+        <img src={props.user.avatar_url} alt='person'/>
+        <div className="card-info">
+          <h3 className="name">{props.user.name}</h3>
+          <p className="username">{props.user.login}</p>
+          <p>Location: {props.user.location}</p>
+          <p>Profile:
+            <a href={props.user.html_url}>{props.user.html_url}</a>
+          </p>
+          <p>Followers: {props.user.followers}</p>
+          <p>Following: {props.user.following}</p>
+          <p>Bio: {props.user.bio}</p>
+        </div>
+      </div>
+    )
+}
+
+export default Card;
