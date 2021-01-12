434d3ab4ee64de6dbe069862683b1928b5a9c47c
Leah Tabush <leahtabush@gmail.com>
2021-01-12 14:01:43 -0800

    completed functionality to display either followers or following, depending on which button is clicked

--- github-user-card/src/Usercard.js
+++ github-user-card/src/Usercard.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Usercard extends Component {
+  state = {
+    followDetails: [],
+    displayedUsers: [],
+  }
+
+  handleClick = e => {
+    e.preventDefault();
+    console.log(`${e.target.name} button was clicked`)
+    
+    axios.get(`https://api.github.com/users/leahfern/${e.target.name}`)
+      .then(res => {
+        this.setState({
+          followDetails: e.target.name,
+          displayedUsers: res.data
+        })
+      })
+      .catch(err => console.log(err));
+
+  };
 
   render() {
     return (
@@ -12,18 +32,33 @@ export default class Usercard extends Component {
         <h4>{this.props.user.login}</h4>
         <img src={this.props.user.avatar_url} alt={this.props.user.login} user image/>
         <ul>
-        {this.props.user.location 
-        ? <li>{this.props.user.location}</li>
-        : ''}
-        {this.props.user.company 
-        ? <li>{this.props.user.company}</li>
-        : ''}
-        {this.props.user.bio 
-        ? <li>{this.props.user.bio}</li>
-        : ''}
-        <li>Followers: {this.props.user.followers}</li>
-        <li>Following: {this.props.user.following}</li>
+          {this.props.user.location 
+            ? <li>{this.props.user.location}</li>
+           : ''}
+          {this.props.user.company 
+           ? <li>{this.props.user.company}</li>
+           : ''}
+          {this.props.user.bio 
+           ? <li>{this.props.user.bio}</li>
+            : ''}
         </ul>
+        <button
+          name='followers'
+          className={`follow${this.state.followDetails === 'followers' ? ' selected' : ''}`} 
+          onClick={this.handleClick}>Followers: {this.props.user.followers}
+        </button>
+        <button
+          name='following'
+          onClick={this.handleClick}>
+          Following: {this.props.user.following}
+        </button>
+        <div className='followDetails'>
+            {this.state.displayedUsers.map(user => {
+              return (<div key={user.id} className='subUser'>
+                <img src={user.avatar_url} alt={`${user.login}`}/>
+              </div>)
+            })}
+        </div>
 
       </div>
     )
