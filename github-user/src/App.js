8e5b16b42423df0ef076c8fe5b25c5896abbbbdb
Michelle Chen <mchen0062@gmail.com>
2021-01-12 15:44:36 -0800

    Created usercard. Need to map over array of followers

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import UserCard from './UserCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+state = {
+  user : [],
+}
+
+
+componentDidMount(){
+axios.get("https://api.github.com/users/duointherain")
+.then(res => {
+  console.log(res.data)
+  this.setState({
+    user: res.data
+  })
+})
+.catch(err => {
+  console.log('you have an error :(');
+});
+}
+
+  render() {
+    return (
+      <div className="App">
+        {(this.state.user.length === 0)?<h3>Gitting Users...</h3>:<UserCard user={this.state.user}/>}
+      </div>
+    );
+  }
 }
 
 export default App;
