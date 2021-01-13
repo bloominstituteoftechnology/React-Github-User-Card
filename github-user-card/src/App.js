e2fdabd6a7d171b7d23f02ea2a0ab6cc3fe3fa00
Leah Tabush <leahtabush@gmail.com>
2021-01-13 07:24:48 -0800

    can now click on followers or following to see their profile. also added redirect to users profile when avatar is clicked.

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -6,23 +6,63 @@ import axios from 'axios'
 
 export default class App extends Component {
   state = {
-    user: {}
+    userInfo: {},
+    inputValue: '',
+    currentUser: 'leahfern'
   }
   componentDidMount() {
-    axios.get('https://api.github.com/users/leahfern')
+    axios.get(`https://api.github.com/users/${this.state.currentUser}`)
       .then(res => {
         this.setState({
-          user: res.data
+          userInfo: res.data
         })
       })
       .catch(err => console.log(err))
   }
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.currentUser !== this.state.currentUser) {
+      axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+      .then(res => {
+        this.setState({
+          userInfo: res.data
+        })
+      })
+      .catch(err => console.log(err))
+    }
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      currentUser: this.state.inputValue,
+      inputValue: '',
+    })
+  }
+  handleChange= e => {
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+
+  setNewMain = username => {
+    this.setState({
+      currentUser: username
+    })
+  }
 
   render() {
     return (
-      <div>
+      <div className='App'>
         <h1>Github User Info</h1>
-        <Usercard user={this.state.user}/>
+        <div className='changeUser'>
+          <p>Try another user?</p>
+          <form onSubmit={this.handleSubmit}>
+            <input type='text' placeholder='github username' value={this.state.inputValue} onChange={this.handleChange}/>
+            <button>send</button>
+          </form>
+        </div>
+        <Usercard userInfo={this.state.userInfo} currentUser={this.state.currentUser} setNewMain={this.setNewMain}/>
+        <div className='buffer'></div>
       </div>
     )
   }
--- github-user-card/src/Usercard.js
+++ github-user-card/src/Usercard.js
@@ -1,64 +1,90 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import axios from 'axios';
+import SubUsers from './SubUsers';
 
 export default class Usercard extends Component {
   state = {
     followDetails: [],
-    displayedUsers: [],
+    followers: [],
+    following: []
   }
 
-  handleClick = e => {
-    e.preventDefault();
-    console.log(`${e.target.name} button was clicked`)
-    
-    axios.get(`https://api.github.com/users/leahfern/${e.target.name}`)
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/${this.props.currentUser}/followers`)
+    .then(res => {
+      this.setState({
+        followDetails: 'followers',
+        followers: res.data,
+      })
+    })
+    axios.get(`https://api.github.com/users/${this.props.currentUser}/following`)
+    .then(res => {
+      this.setState({
+        following: res.data
+      })
+    })
+  }
+
+  componentDidUpdate(prevProps) {
+    if(prevProps.currentUser !== this.props.currentUser) {
+      axios.get(`https://api.github.com/users/${this.props.currentUser}/followers`)
       .then(res => {
         this.setState({
-          followDetails: e.target.name,
-          displayedUsers: res.data
+          followDetails: 'followers',
+          followers: res.data,
         })
       })
-      .catch(err => console.log(err));
+      axios.get(`https://api.github.com/users/${this.props.currentUser}/following`)
+      .then(res => {
+        this.setState({
+          following: res.data
+        })
+      })
+    }
+  }
 
+  setFollowersOrFollowing = followDetails => {
+    this.setState(followDetails)
   };
+
+  setNewMainUser = username => {
+    this.props.setNewMain(username)
+  }
+
 
   render() {
     return (
       <div className='Usercard'>
-        { this.props.user.name
-          ? <h2>{this.props.user.name}</h2>
-          : <h2>{this.props.user.login}</h2>
+        { this.props.userInfo.name
+          ? <h2>{this.props.userInfo.name}</h2>
+          : <h2>{this.props.userInfo.login}</h2>
         }
-        <h4>{this.props.user.login}</h4>
-        <img src={this.props.user.avatar_url} alt={this.props.user.login} user image/>
-        <ul>
-          {this.props.user.location 
-            ? <li>{this.props.user.location}</li>
+        { this.props.userInfo.name
+          ? <h4>{this.props.userInfo.login}</h4>
+          : ''
+        }
+        <a href={`https://www.github.com/${this.props.userInfo.login}`} target="_blank" rel='noreferrer'>
+          <img src={this.props.userInfo.avatar_url} alt={this.props.userInfo.login} className='mainUserImg'/>
+        </a>
+        <ul className='info-bottom'>
+          {this.props.userInfo.location 
+            ? <li>{this.props.userInfo.location}</li>
            : ''}
-          {this.props.user.company 
-           ? <li>{this.props.user.company}</li>
+          {this.props.userInfo.company 
+           ? <li>{this.props.userInfo.company}</li>
            : ''}
-          {this.props.user.bio 
-           ? <li>{this.props.user.bio}</li>
+          {this.props.userInfo.bio 
+           ? <li>"{this.props.userInfo.bio}"</li>
             : ''}
         </ul>
-        <button
-          name='followers'
-          className={`follow${this.state.followDetails === 'followers' ? ' selected' : ''}`} 
-          onClick={this.handleClick}>Followers: {this.props.user.followers}
-        </button>
-        <button
-          name='following'
-          onClick={this.handleClick}>
-          Following: {this.props.user.following}
-        </button>
-        <div className='followDetails'>
-            {this.state.displayedUsers.map(user => {
-              return (<div key={user.id} className='subUser'>
-                <img src={user.avatar_url} alt={`${user.login}`}/>
-              </div>)
-            })}
-        </div>
+        <SubUsers 
+          followDetails={this.state.followDetails}
+          setFollowersOrFollowing = {this.setFollowersOrFollowing}
+          userInfo={this.props.userInfo}
+          followers = {this.state.followers}
+          setNewMainUser = {this.setNewMainUser}
+          following = {this.state.following}
+        />
 
       </div>
     )
