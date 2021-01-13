bc3012682a17e018ecd7286bec8ce77e8a692c95
Michelle Chen <mchen0062@gmail.com>
2021-01-12 16:47:35 -0800

    completed assignment

--- github-user/src/App.js
+++ github-user/src/App.js
@@ -7,15 +7,18 @@ class App extends React.Component {
 
 state = {
   user : [],
+  followers: []
 }
 
 
 componentDidMount(){
-axios.get("https://api.github.com/users/duointherain")
-.then(res => {
-  console.log(res.data)
+Promise.all([axios.get("https://api.github.com/users/duointherain"), axios.get("https://api.github.com/users/duointherain/followers")])
+.then(([res1, res2]) => {
+  console.log("res1", res1.data)
+  console.log("res2", res2.data)
   this.setState({
-    user: res.data
+    user: res1.data,
+    followers: res2.data
   })
 })
 .catch(err => {
@@ -26,7 +29,7 @@ axios.get("https://api.github.com/users/duointherain")
   render() {
     return (
       <div className="App">
-        {(this.state.user.length === 0)?<h3>Gitting Users...</h3>:<UserCard user={this.state.user}/>}
+        {(this.state.user.length === 0)?<h3>Gitting Users...</h3>:<UserCard user={this.state.user} followers={this.state.followers}/>}
       </div>
     );
   }
--- github-user/src/UserCard.js
+++ github-user/src/UserCard.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Followers from './Followers';
 
 class UserCard extends React.Component {
+
   render() {
     return (
       <>
@@ -16,6 +18,11 @@ class UserCard extends React.Component {
                 {this.props.user.login}</a></p>
             <p>Public Repos: {this.props.user.public_repos}</p>
             <p>Followers: {this.props.user.followers}</p>
+            {
+                this.props.followers.map(follower => (
+                    <Followers key={follower.id} login={follower.login} pic={follower.avatar_url}/>
+                ))
+            }
           </div>
         </div>
       </>
