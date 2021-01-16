1112885ed9dfeb80da7896c59cd41eccfccfd153
george-hatzigeorgio <georgehatzigeorgio@zohomail.com>
2021-01-16 17:38:30 -0500

    finished product

--- github-usercard/src/FollowersCard.js
+++ github-usercard/src/FollowersCard.js
@@ -2,50 +2,44 @@ import axios from "axios"
 import React,{Component} from 'react'
 
 export default class FollowersCard extends Component {
-    constructor(){
-        super();
-    this.state ={
-        user:[],
-        login:"",
-        avatar_url:""
-        
-    }
-}      
-          
-componentDidMount(){
-   axios.get("https://api.github.com/users/tetondan/followers")
-        .then((res)=>{
-            console.log(res)
-            this.setState({user:res.data})
-        })
-         .catch((err)=>{
+  constructor() {
+    super();
+    this.state = {
+      user: [],
+      login: "",
+      avatar_url: "",
+    };
+  }
 
-            console.log(err)
-         })
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/users/tetondan/followers")
+      .then((res) => {
+        console.log(res);
+        this.setState({ user: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-
-
-        }
-
-    render(){
-
-        return (
-         <section className="cards-container">
-       { this.state.user.map((user) => (
+  render() {
+    return (
+      <section>
+        {this.state.user.map((user) => (
           <div className="card-container">
             <div className="image-container">
               <img src={user.avatar_url} />
             </div>
-              <div className="cards-title">
-                <h3>Login:    {user.login}</h3>
-              
+            <div className="cards-title">
+              <h3>Login: {user.login}</h3>
             </div>
           </div>
-        ))};
-        </section>
-        )
-}
-
+        ))}
+        ;
+      </section>
+    );
+  }
 }
 
         
