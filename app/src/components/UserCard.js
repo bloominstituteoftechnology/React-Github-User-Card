f97bc9a0219ff356169cae38928eb22a8b13df36
Alan Mir <alanpaulphotos@gmail.com>
2021-01-17 23:36:21 -0500

    Created the UserCard with reactstrap

--- app/src/components/UserCard.js
+++ app/src/components/UserCard.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle
+} from 'reactstrap';
 
-const UserCard = (props) => {};
+
+const UserCard = (props) => {
+    
+
+    return (
+        <div>
+          <Card>
+            <CardImg top width="100%" src={props.user.avatar_url} alt="Card image cap" />
+            <CardBody>
+              <CardTitle>{props.user.login}</CardTitle>
+              <CardTitle>{props.user.name}</CardTitle>
+              <CardSubtitle>Followers: {props.user.followers}</CardSubtitle>
+              <CardText>{props.user.bio}</CardText>
+            </CardBody>
+          </Card>
+        </div>
+      );
+};
 
 export default UserCard;
