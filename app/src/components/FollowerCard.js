9308f8f1aa983fb2c7fb34d1ace12e113f69cc04
Alan Mir <alanpaulphotos@gmail.com>
2021-01-17 23:32:22 -0500

    Created Follower cards using react strap

--- app/src/components/FollowerCard.js
+++ app/src/components/FollowerCard.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import {
+    Card, CardImg, CardBody,
+    CardTitle, Button, CardSubtitle
+  } from 'reactstrap';
 
-const FollowerCard = () => {};
+
+const FollowerCard = (props) => {
+
+
+    return (
+        <div className="followerDiv" >
+            {props.followers.map((follower) => {
+                return (
+                    <Card className="follower" key={follower.id} >
+                    <CardImg top width="100%" src={follower.avatar_url} alt="Card image cap" />
+                    <CardBody>
+                        <CardTitle>{follower.login}</CardTitle>
+                        <CardSubtitle>{follower.name}</CardSubtitle>
+                    </CardBody>
+                    <Button color="info" >Visit</Button>
+                    </Card>
+                )
+            })}
+        </div>
+    );
+};
 
 export default FollowerCard;
