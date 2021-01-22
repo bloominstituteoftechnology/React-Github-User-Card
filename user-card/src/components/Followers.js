51047254bc9f8db39d8314b77be989823922d674
Ryan Clark <rbclark244@gmail.com>
2021-01-21 22:06:36 -0500

    MVP

---
+++ user-card/src/components/Followers.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./componentStyles.css";
+
+function Followers({ followers }) {
+  return (
+    <>
+      {followers.map((follower) => (
+        <div key={follower.id} className="info">
+          <img
+            className="followPic"
+            src={follower.avatar_url}
+            alt={follower.name}
+          />
+          <div className="lines1">{follower.name}</div>
+          <div className="lines2">{follower.login}</div>
+          <div className="lines3">{follower.type}</div>
+        </div>
+      ))}
+    </>
+  );
+}
+export default Followers;
