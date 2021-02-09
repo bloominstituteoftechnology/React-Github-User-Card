ed592386cc86ca3a56e90edc09fa68d8eae618c9
Jonathan-Majors <izzymajors@gmail.com>
2021-02-09 17:25:18 -0600

    added username

--- User-Card/src/App.js
+++ User-Card/src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios'
 import './App.css';
+import { render } from 'react-dom';
 
-function App() {
-  return (
+//https://api.github.com/users/izzymajors
+
+
+class App extends React.Component {
+  state =  {
+    users: [],
+    login:"",
+    name: "",
+    company: "",
+    type: "",
+    url:"",
+  };
+
+  componentDidMount() {
+    axios
+    .get("https://api.github.com/users/izzymajors")
+    .then(res => {
+      this.setState({
+        users: res.data.message,
+        login: res.data.login,
+        name: res.data.name,
+        company: res.data.company,
+        type: res.data.type,
+        url: res.data.url,
+
+      })
+    })
+    .catch((err) => console.log(err));
+  }
+
+  handleChange = () => {
+    this.setState({
+     
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+    .get(`https://api.github.com/users/${this.state.login}`)
+    .then( res => {
+      this.setState({
+        name: res.data.name,
+        company: res.data.company,
+        type: res.data.type,
+        url: res.data.url,
+      })
+    })
+  }
+
+
+
+render() {
+  console.log('rendering');
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <h1>Hello User</h1>
+      <form>
+        <input value={this.state.users}
+         onChange={this.handleChange}/>
+      </form>
 
+    </div>
+  )
+}
+}
 export default App;
