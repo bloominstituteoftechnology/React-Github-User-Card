ccc30605139a348a74902e3265afab5be92954c7
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-09 16:12:51 -0700

    getting my github data

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import axios from 'axios';
 import './App.css';
+import Followers from './components/Followers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    github: []
+  }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/Stone98')
+      .then((res) => {
+        this.setState({
+          github: res.data
+        })
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    return (
+      <>
+        <div className='card'>
+          <img src={this.state.github.avatar_url} alt='user-avatar' />
+          <div className='card-info'>
+            <h3 className="name">{this.state.github.name}</h3>
+            <p className="username">{this.state.github.login}</p>
+            <p>Location: {this.state.github.location}</p>
+            <p>Profile: <a href={this.state.github.html_url}>{this.state.github.html_url}</a></p>
+            <p>Followers: {this.state.github.followers}</p>
+            <p>Following: {this.state.github.following}</p>
+            <p>Bio: {this.state.github.bio}</p>
+          </div>
+        </div>
+        <div className='card'>
+          <h2 className='name'>{this.state.github.login}'s Followers:</h2>
+        </div>
+        <div>
+          <Followers />
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
+
---
+++ react-github-usercard/src/components/Followers.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Followers extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Followers
