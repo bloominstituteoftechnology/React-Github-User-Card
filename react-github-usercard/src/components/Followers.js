fa012159aaea0acc239edc23ca25a94803c2f01b
BluKnight98 <70171431+BluKnight98@users.noreply.github.com>
2021-02-09 16:21:33 -0700

    getting followers' data

--- react-github-usercard/src/components/Followers.js
+++ react-github-usercard/src/components/Followers.js
@@ -1,11 +1,37 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 
 export class Followers extends Component {
+    state = {
+        followers: []
+    }
+
+    componentDidMount() {
+        axios
+            .get('https://api.github.com/users/Stone98/followers')
+            .then((res) => {
+                this.setState({
+                    followers: res.data
+                })
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
     render() {
         return (
-            <div>
-
-            </div>
+            <>
+                {this.state.followers.map(follower => (
+                    <div className="card" key={follower.id}>
+                        <img src={follower.avatar_url} alt={follower.login} />
+                        <div className="card-info">
+                            <p className="username">{follower.login}</p>
+                            <p>Profile: <a href={follower.html_url}>{follower.html_url}</a></p>
+                        </div>
+                    </div>
+                ))}
+            </>
         )
     }
 }
