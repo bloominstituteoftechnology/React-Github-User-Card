b2308083309b8365a4c4dc3c0ca7dc96952226d4
hdm007 <harry.mardirossian13@gmail.com>
2021-02-10 01:09:49 -0500

    app shell set up, using class component, and axios on CDM

--- usercards/src/App.js
+++ usercards/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    github: {},
+    error: "",
+  };
+
+  componentDidMount() {
+    axios
+    .get(`https://api.github.com/users/hdm007`)
+    .then((res) => {
+      this.setState({
+        github: res.data,
+      });
+    })
+    .catch((err) => {
+      this.setState({
+        error: err.message,
+      })
+    })
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <h1>Github User Card</h1>
+        {this.state.github === {} ? (
+          <p>Loading info...</p>
+        ) : <p> Yay! State is loaded! </p>}
+      </React.Fragment>
+    )
+  }
+
+
 }
 
 export default App;
