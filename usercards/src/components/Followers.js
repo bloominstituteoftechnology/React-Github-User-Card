e331c2ac149d25361fa5b023512b76f66be17963
hdm007 <harry.mardirossian13@gmail.com>
2021-02-10 01:39:18 -0500

    MVP

---
+++ usercards/src/components/Followers.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Followers extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+
+        return (
+            <React.Fragment>
+                <h1>sup</h1>
+            </React.Fragment>
+        )
+    }
+
+}
--- usercards/src/components/User.js
+++ usercards/src/components/User.js
@@ -1,13 +1,23 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class User extends Component {
     constructor(props) {
         super(props);
-        this.state = {loaded: false};
+        this.state = {followers: []}
     }
 
     componentDidMount() {
-        console.log(this.props)
+        axios
+        .get("https://api.github.com/users/HDM007/followers")
+        .then((res) => {
+            this.setState({
+                followers: res.data
+            })
+        })
+        .catch((err) => {
+            console.log(err.message)
+        })
     }
 
     render() {
@@ -16,6 +26,12 @@ class User extends Component {
             <React.Fragment>
                 <p>Name: {this.props.data.name}</p>
                 <p>Activity: {this.props.data.updated_at}</p>
+                <p>Followers Below: </p>
+                <ul>
+                {this.state.followers.map(follower => {
+                    return <li>{follower.login}</li>
+                })}
+                </ul>
             </React.Fragment>
         )
     }
