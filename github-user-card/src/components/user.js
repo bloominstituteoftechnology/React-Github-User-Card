89dbd0ec0306e6a5bee67be19d55629138e2d922
Kelsey Nielsen <kelseywnielsen@gmail.com>
2021-02-12 12:21:06 -0700

    return info of user through props in user component

--- github-user-card/src/components/user.js
+++ github-user-card/src/components/user.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 class User extends React.Component {
-
   render(){
-    return (
-      <div>
 
+    const { currentUser } = this.props;
+
+    return (
+      <div className='user-card'>
+          {console.log('props', this.props)}
+          <img src={currentUser.avatar_url} alt='avatar' />
+          <p>Name: {currentUser.name}</p>
+          <p>Username: {currentUser.login}</p>
+          <p>Bio: {currentUser.bio}</p>
+          <p>Location: {currentUser.location}</p>
+          <p>Company: {currentUser.company}</p>
+          <p>Blog: {currentUser.blog}</p>
+          <p>Email: {currentUser.email}</p>
+          <p>Followers: {currentUser.followers}</p>
+          <p>Following: {currentUser.following}</p>
       </div>
     );
   }
