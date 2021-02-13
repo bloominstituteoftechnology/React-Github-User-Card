7287e02ac9841ab4f332e34c2d514b9ddb6341b0
Rex-1031 <64785141+Rex-1031@users.noreply.github.com>
2021-02-13 13:01:17 -0500

    GitStats component created, filled with dummy data, and added to the app

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserInfo from './components/UserInfo'
+import GitStats from './components/GitStats'
 import './App.css';
 
 class App extends React.Component {
@@ -15,12 +16,17 @@ class App extends React.Component {
         <h1>Github User</h1>
 
         <div className="userCard">
-          <div>
+          <div className="CardHeader">
             <img src="" alt="User Avatar" />
             <h3>User Name</h3>
           </div>
+
           <div className='userInfo'>
           <UserInfo />
+          </div>
+
+          <div className="gitStats">
+            <GitStats />
           </div>
         </div>
       </div> );
---
+++ user-card/src/components/GitStats.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const GitStats = () => {
+    return ( 
+        <div>
+            <p>Repos</p>
+            <p>Orgs</p>
+            <p>gists</p>
+            <p>Contribution Graph</p>
+        </div>
+     );
+}
+ 
+export default GitStats
