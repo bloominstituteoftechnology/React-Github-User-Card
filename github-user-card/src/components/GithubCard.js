01e02735801e83e2f4dacc10c6e216bdb87c82c1
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2021-02-13 17:38:18 -0700

    added some styling

--- github-user-card/src/components/GithubCard.js
+++ github-user-card/src/components/GithubCard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import '../Github-Card.css'
 import Avatar from '@material-ui/core/Avatar';
+import Card from '@material-ui/core/Card';
 import axios from 'axios'
 
 
@@ -21,14 +23,20 @@ export class GithubCard extends Component {
 
   render() {
     return (
-      <div>
-        <h1> {this.state.user.login} <Avatar img src={this.state.user.avatar_url}/></h1>
-        <p><strong>Full Name:</strong>{this.state.user.name}</p>
+
+      <div className="github__card">
+          <Card>
+          <div className="avatar">
+        <Avatar img src={this.state.user.avatar_url} />
+        </div>
+        <h1> {this.state.user.login} </h1>
+        <p><strong>Full Name: </strong>{this.state.user.name}</p>
         <p><strong>Bio: </strong>{this.state.user.bio}</p>
         <p><strong>Location: </strong>{this.state.user.location} </p>
-        <p><strong>Current Company:</strong>{this.state.user.company}</p>
-        <p><strong>Following:</strong> {this.state.user.following}</p>
-        <p><strong>Followers:</strong>{this.state.user.followers}</p>
+        <p><strong>Current Company: </strong>{this.state.user.company}</p>
+        <p><strong>Following: </strong> {this.state.user.following}</p>
+        <p><strong>Followers: </strong>{this.state.user.followers}</p>
+        </Card>
       </div>
     )
   }
