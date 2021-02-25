18056d7c4f9be267462053fbfb0631b5836799c8
D'Artagnan von Pingel <dvonpingel@gmail.com>
2021-02-25 11:12:56 -0700

    Cleaned up README and files

--- usercard/src/App.js
+++ usercard/src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import axios from 'axios';
-import Card from './components/card'
-import styled from 'styled-components'
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import Card from "./components/card";
+import styled from "styled-components";
 
 class App extends React.Component {
   constructor() {
@@ -11,66 +10,66 @@ class App extends React.Component {
     this.state = {
       user: [],
       followers: [],
-      inputValue: '',
-    }
+      inputValue: "",
+    };
   }
 
   componentDidMount() {
     axios
-    .get(`https://api.github.com/users/dvonpingel`)
-    .then(res => {
-      this.setState({user: res.data})
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .get(`https://api.github.com/users/dvonpingel`)
+      .then((res) => {
+        this.setState({ user: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     axios
-    .get('https://api.github.com/users/dvonpingel/followers')
-    .then(res => {
-      this.setState({followers: res.data})
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .get("https://api.github.com/users/dvonpingel/followers")
+      .then((res) => {
+        this.setState({ followers: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
-  handleChanges = evt => {
+  handleChanges = (evt) => {
     this.setState({
       inputValue: evt.target.value,
-    })
-  }
+    });
+  };
 
-  handleSubmit = evt => {
+  handleSubmit = (evt) => {
     evt.preventDefault();
     axios
-    .get(`https://api.github.com/users/${this.state.inputValue}`)
-    .then(res => {
-      this.setState({user: res.data})
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      .get(`https://api.github.com/users/${this.state.inputValue}`)
+      .then((res) => {
+        this.setState({ user: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     axios
-    .get(`https://api.github.com/users/${this.state.inputValue}/followers`)
-    .then(res => {
-      this.setState({followers: res.data})
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
+      .get(`https://api.github.com/users/${this.state.inputValue}/followers`)
+      .then((res) => {
+        this.setState({ followers: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   render() {
     return (
       <div>
         <div>
-          <Card user={this.state.user} followers={this.state.followers}/>
+          <Card user={this.state.user} followers={this.state.followers} />
         </div>
         <ContainerDiv>
           <StyledForm onSubmit={this.handleSubmit}>
             <H3>Enter Github Username</H3>
             <StyledInput
-              type='text'
+              type="text"
               value={this.state.inputValue}
               onChange={this.handleChanges}
             ></StyledInput>
@@ -90,7 +89,7 @@ const ContainerDiv = styled.div`
   box-shadow: -20px 20px #698996;
 `;
 const StyledForm = styled.form`
-  background-color: #97B1A6;
+  background-color: #97b1a6;
   width: 300px;
   height: 100px;
   margin: 10px auto 0 auto;
@@ -103,7 +102,7 @@ const StyledForm = styled.form`
 
 const StyledInput = styled.input`
   box-sizing: border-box;
-  background-color: #97B1A6;
+  background-color: #97b1a6;
   border: none;
   border-bottom: 3px solid white;
   height: 40px;
@@ -126,12 +125,12 @@ const Button = styled.button`
   font-weight: bold;
   color: white;
   font-size: 1rem;
-  transition: .2s;
+  transition: 0.2s;
 
   &:hover {
     background-color: white;
     color: #407076;
-    transition: .2s;
+    transition: 0.2s;
     cursor: pointer;
   }
 `;
