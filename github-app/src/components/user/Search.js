51f2dd6615612619410e9ae0bbeb68a584b730c9
Mosharraf-Musa <mosharrafny@gmail.com>
2021-03-29 13:22:34 -0400

    search user added

--- github-app/src/components/user/Search.js
+++ github-app/src/components/user/Search.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react';
+import PropTypes from 'prop-types'
 
 function Search() {
+    const [text, setText] = useState('');
+    const onSubmit =e =>{
+        e.preventDefault();
+        
+    }
+    const onChange =e=> setText(e.target.value);
     return (
         <div>
-            
+          <form>
+              <input
+              type ='submit'
+              name='text'
+              placeholder='Search Users...'
+              onChange={onChange}
+              />
+              <input
+              type='submit'
+              value='Search'
+              className='btn btn-light btn-block' 
+              />
+              </form>  
+              {showClear && (
+                  <button className='btn btn-light btn-block' onClick={clearUsers}>
+                      Clear
+                  </button>
+              )}
         </div>
     )
 }
