10e96d3a6e4868eac00ff41d3837281f1b9e75a8
Hutch Crowley <tothecoda@gmail.com>
2021-05-16 02:46:05 -0500

    Update fix again

--- src/App.js
+++ src/App.js
@@ -6,43 +6,40 @@ import FollowersList from './components/FollowersList'
 import UserCard from './components/UserCard'
 
 const App = () => {
-	const [ user, setUser ] = useState({})
-	const [ followers, setFollowers ] = useState([])
-	const [ query, setQuery ] = useState('hutchcrowley')
+  const [user, setUser] = useState({})
+  const [followers, setFollowers] = useState([])
+  const [query, setQuery] = useState('hutchcrowley')
 
-	useEffect(
-		() => {
-			axios
-				.get(`https://api.github.com/users/${query}`)
-				.then(res => {
-					console.log('Result of search API call in CDU: ', res.data)
-					setUser(res.data)
-				})
-				.catch(err => console.log('Error: ', err))
-		},
-		[ query ],
-	)
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/users/${query}`)
+      .then(res => {
+        console.log('Result of search API call in CDU: ', res.data)
+        setUser(res.data)
+      })
+      .catch(err => console.log('Error: ', err))
+  }, [query])
 
-	useEffect(() => {
-		axios
-			.get('https://api.github.com/users/hutchcrowley/followers')
-			.then(res => {
-				console.log(res.data)
-				setFollowers(res.data)
-			})
-			.catch(err => console.log('Error: '.err))
-	}, [])
+  useEffect(() => {
+    axios
+      .get('https://api.github.com/users/hutchcrowley/followers')
+      .then(res => {
+        console.log(res.data)
+        setFollowers(res.data)
+      })
+      .catch(err => console.log('Error: ', err))
+  }, [])
 
-	return (
-		<Container fluid={true} className='App'>
-			<Col className='user-column'>
-				<UserCard user={user} />
-			</Col>
-			<Col xs={3} className='follower-column'>
-				<FollowersList followers={followers} setQuery={setQuery} />
-			</Col>
-		</Container>
-	)
+  return (
+    <Container className='App'>
+      <Col className='user-column'>
+        <UserCard user={user} />
+      </Col>
+      <Col xs={3} className='follower-column'>
+        <FollowersList followers={followers} setQuery={setQuery} />
+      </Col>
+    </Container>
+  )
 }
 
 export default App
